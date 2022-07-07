2888ec8ec56bc0013f23e9e795a6fd843f592489
Amy J. Ko <ajko@uw.edu>
2022-07-07 06:59:04 -0700

    Fixed annoying window sizing.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,12 +4,13 @@
 
 <style>
     div {
-        width: 100vw;
+        width: 100%;
         height: auto;
         border: var(--wordplay-border-width) solid var(--wordplay-border-color);
         padding: var(--wordplay-spacing);
         background: var(--wordplay-chrome);
         border-bottom-left-radius: var(--wordplay-border-radius);
         border-bottom-right-radius: var(--wordplay-border-radius);
+        box-sizing: border-box;
     }
 </style>
