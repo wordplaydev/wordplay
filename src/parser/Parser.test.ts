94dce5f61fb4ddde67929d6371e612da32d90fb1
Amy J. Ko <ajko@uw.edu>
2022-07-08 18:27:02 -0700

    Tested borrows.

--- src/parser/Parser.test.ts
+++ src/parser/Parser.test.ts
@@ -1,9 +1,24 @@
+import Block from "./Block";
+import Borrow from "./Borrow";
 import { parse } from "./Parser";
 import Program from "./Program";
+import Unparsable from "./Unparsable";
 
 test("Parse programs", () => {
 
     expect(parse("")).toBeInstanceOf(Program);
     expect(parse("hi")).toBeInstanceOf(Program);
+
+})
+
+test("Parse borrows", () => {
+
+    const good = parse("↓ mouse");
+    expect(good.borrows).toHaveLength(1);
+    expect(good.borrows[0]).toBeInstanceOf(Borrow);
+
+    const bad = parse("↓");
+    expect(bad.borrows).toHaveLength(1);
+    expect(bad.borrows[0]).toBeInstanceOf(Unparsable);
 
 })
