222eed9de082207a521b742ae22f22889c3d1ccd
Amy J. Ko <ajko@uw.edu>
2022-07-08 19:05:49 -0700

    More borrow precision.

--- src/parser/Parser.test.ts
+++ src/parser/Parser.test.ts
@@ -2,6 +2,7 @@ import Block from "./Block";
 import Borrow from "./Borrow";
 import { parse } from "./Parser";
 import Program from "./Program";
+import { TokenType } from "./Token";
 import Unparsable from "./Unparsable";
 
 test("Parse programs", () => {
@@ -16,6 +17,7 @@ test("Parse borrows", () => {
     const good = parse("↓ mouse");
     expect(good.borrows).toHaveLength(1);
     expect(good.borrows[0]).toBeInstanceOf(Borrow);
+    expect((good.borrows[0] as Borrow).name.is(TokenType.NAME)).toBe(true);
 
     const bad = parse("↓");
     expect(bad.borrows).toHaveLength(1);
