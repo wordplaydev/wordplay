cbaa5bd6c674b302362542bebb65ed123e72e89f
Amy J. Ko <ajko@uw.edu>
2022-07-11 15:29:23 -0700

    Checked binary operator types.

--- src/parser/BinaryOperation.ts
+++ src/parser/BinaryOperation.ts
@@ -1,6 +1,9 @@
-import type Conflict from "./Conflict";
+import BooleanType from "./BooleanType";
+import Conflict from "./Conflict";
 import Expression from "./Expression";
+import MeasurementType from "./MeasurementType";
 import type Program from "./Program";
+import { SemanticConflict } from "./SemanticConflict";
 import type { Token } from "./Token";
 import type Type from "./Type";
 import UnknownType from "./UnknownType";
@@ -24,10 +27,68 @@ export default class BinaryOperation extends Expression {
         return [ this.left, this.operator, this.right ];
     }
 
-    getConflicts(program: Program): Conflict[] { return []; }
+    getConflicts(program: Program): Conflict[] { 
+
+        const leftType = this.left.getType(program);
+        const rightType = this.right instanceof Expression ? this.right.getType(program) : undefined;
+
+        if(rightType === undefined) return [];
+
+        // Left and right must be numbers
+        switch(this.operator.text) {
+            case "-":
+            case "+":
+            case "×":
+            case "*":
+            case "·":
+            case "÷":
+            case "%":
+            case "^":
+            case "<":
+            case ">":
+            case "≤":
+            case "≥":
+            case "=":
+            case "≠":
+                if(!(leftType instanceof MeasurementType) || !(rightType instanceof MeasurementType))
+                    return [ new Conflict(this, SemanticConflict.ARITHMETIC_REQUIRES_NUMBERS)]
+                // Need to verify units
+                break;
+            case "∧":
+            case "∨":
+                if(!(leftType instanceof BooleanType) || !(rightType instanceof BooleanType))
+                    return [ new Conflict(this, SemanticConflict.LOGIC_REQUIRES_BOOLEANS)]
+                break;
+        }
+
+        return [];
+    
+    }
 
     getType(program: Program): Type {
-        return new UnknownType(this);
+        switch(this.operator.text) {
+            case "-":
+            case "+":
+            case "×":
+            case "*":
+            case "·":
+            case "÷":
+            case "%":
+            case "^":
+                // This is wrong; it doesn't account for units.
+                return this.left.getType(program);
+            case "<":
+            case ">":
+            case "≤":
+            case "≥":
+            case "=":
+            case "≠":
+            case "∧":
+            case "∨":
+                return new BooleanType();
+            default:
+                return new UnknownType(this);
+        }
     }
 
 }
