9114b217f77c064a8c23b9684bb486afa9e32b60
Amy J. Ko <ajko@uw.edu>
2022-07-12 08:51:04 -0700

    Checked that select columns are names.

--- src/parser/Select.ts
+++ src/parser/Select.ts
@@ -2,10 +2,12 @@ import type { Token } from "./Token";
 import Expression from "./Expression";
 import type Row from "./Row";
 import type Program from "./Program";
-import type Conflict from "./Conflict";
+import Conflict from "./Conflict";
 import type Type from "./Type";
 import UnknownType from "./UnknownType";
 import type Unparsable from "./Unparsable";
+import { SemanticConflict } from "./SemanticConflict";
+import Name from "./Name";
 
 export default class Select extends Expression {
     
@@ -26,7 +28,19 @@ export default class Select extends Expression {
 
     getChildren() { return [ this.table, this.select, this.row, this.query ]; }
 
-    getConflicts(program: Program): Conflict[] { return []; }
+    getConflicts(program: Program): Conflict[] { 
+        
+        const conflicts: Conflict[] = [];
+
+        // The columns in a select must be names.
+        this.row.cells.forEach(cell => {
+            if(!(cell.expression instanceof Name))
+                conflicts.push(new Conflict(cell, SemanticConflict.SELECT_COLUMNS_MUST_BE_NAMES))
+        });
+
+        return conflicts;
+    
+    }
 
     getType(program: Program): Type {
         // A table type based on the row
