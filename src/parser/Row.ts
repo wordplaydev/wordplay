8731d86e819f864a241f60b2d118675a33072fc8
Amy J. Ko <ajko@uw.edu>
2022-07-12 09:33:15 -0700

    Checked table update column names and types.

--- src/parser/Row.ts
+++ src/parser/Row.ts
@@ -2,6 +2,7 @@ import Node from "./Node";
 import type Cell from "./Cell";
 import type Program from "./Program";
 import type Conflict from "./Conflict";
+import Name from "./Name";
 
 export default class Row extends Node {
 
--- src/parser/Update.ts
+++ src/parser/Update.ts
@@ -2,9 +2,12 @@ import type { Token } from "./Token";
 import Expression from "./Expression";
 import type Row from "./Row";
 import type Program from "./Program";
-import type Conflict from "./Conflict";
-import type Type from "./Type";
+import Conflict from "./Conflict";
+import Type from "./Type";
 import type Unparsable from "./Unparsable";
+import Bind from "./Bind";
+import { SemanticConflict } from "./SemanticConflict";
+import TableType from "./TableType";
 
 export default class Update extends Expression {
     
@@ -25,7 +28,30 @@ export default class Update extends Expression {
 
     getChildren() { return [ this.table, this.update, this.row, this.query ]; }
 
-    getConflicts(program: Program): Conflict[] { return []; }
+    getConflicts(program: Program): Conflict[] { 
+        
+        const conflicts: Conflict[] = [];
+
+        const tableType = this.table.getType(program);
+
+        // The columns in an update must be binds with expressions.
+        this.row.cells.forEach(cell => {
+            if(!(cell.expression instanceof Bind && cell.expression.value !== undefined && cell.expression.names.length === 1))
+                conflicts.push(new Conflict(cell, SemanticConflict.EXPECTED_UPDATE_SINGLE_NAME_VALUED_BIND))
+            else if(tableType instanceof TableType) {
+                const columnType = tableType.getColumnNamed(cell.expression.names[0].name.text);
+                if(columnType === undefined)
+                    conflicts.push(new Conflict(cell, SemanticConflict.UNKNOWN_TABLE_COLUMN));
+                else if(columnType.type instanceof Type) {
+                    if(!columnType.type.isCompatible(program, cell.expression.getType(program)))
+                        conflicts.push(new Conflict(cell, SemanticConflict.TABLE_UPDATE_TYPE_MISMATCH));
+                }
+            }
+        });
+
+        return conflicts; 
+    
+    }
 
     getType(program: Program): Type {
         // The type of an update is the type of its table
