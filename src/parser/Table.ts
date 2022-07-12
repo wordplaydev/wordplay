d4a4bb6ad03711e7fc29c5ceb58517ad25861fcb
Amy J. Ko <ajko@uw.edu>
2022-07-12 08:35:19 -0700

    Require types on table columns.

--- src/parser/Table.ts
+++ src/parser/Table.ts
@@ -1,12 +1,15 @@
 import type Column from "./Column";
 import type Row from "./Row";
 import type Program from "./Program";
-import type Conflict from "./Conflict";
+import Conflict from "./Conflict";
 import Expression from "./Expression";
 import type Type from "./Type";
 import TableType from "./TableType";
 import UnknownType from "./UnknownType";
 import ColumnType from "./ColumnType";
+import { SemanticConflict } from "./SemanticConflict";
+import Unparsable from "./Unparsable";
+import type Bind from "./Bind";
 
 export default class Table extends Expression {
     
@@ -22,7 +25,17 @@ export default class Table extends Expression {
 
     getChildren() { return [ ...this.columns, ...this.rows ]; }
 
-    getConflicts(program: Program): Conflict[] { return []; }
+    getConflicts(program: Program): Conflict[] { 
+    
+        const conflicts = [];
+
+        // Columns must all have types.
+        if(!(this.columns.map(c => c.bind) as (Bind|Unparsable)[]).every(bind => bind instanceof Unparsable || !(bind.getType(program) instanceof UnknownType)))
+            conflicts.push(new Conflict(this, SemanticConflict.COLUMNS_MUST_BE_TYPED))
+
+        return conflicts; 
+    
+    }
 
     getType(program: Program): Type {
         const columnTypes = this.columns.map(c => new ColumnType(c.getType(program)));
