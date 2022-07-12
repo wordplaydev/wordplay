f1119f84b0c65b2ff1cc734ec2f6114e7c3af089
Amy J. Ko <ajko@uw.edu>
2022-07-12 09:47:19 -0700

    Checked update query.

--- src/parser/Update.ts
+++ src/parser/Update.ts
@@ -8,6 +8,7 @@ import type Unparsable from "./Unparsable";
 import Bind from "./Bind";
 import { SemanticConflict } from "./SemanticConflict";
 import TableType from "./TableType";
+import BooleanType from "./BooleanType";
 
 export default class Update extends Expression {
     
@@ -34,20 +35,26 @@ export default class Update extends Expression {
 
         const tableType = this.table.getType(program);
 
-        // The columns in an update must be binds with expressions.
         this.row.cells.forEach(cell => {
+            // The columns in an update must be binds with expressions.
             if(!(cell.expression instanceof Bind && cell.expression.value !== undefined && cell.expression.names.length === 1))
                 conflicts.push(new Conflict(cell, SemanticConflict.EXPECTED_UPDATE_SINGLE_NAME_VALUED_BIND))
             else if(tableType instanceof TableType) {
                 const columnType = tableType.getColumnNamed(cell.expression.names[0].name.text);
+                // The named table column must exist.
                 if(columnType === undefined)
                     conflicts.push(new Conflict(cell, SemanticConflict.UNKNOWN_TABLE_COLUMN));
+                // The types of the bound values must match the column types.
                 else if(columnType.type instanceof Type) {
                     if(!columnType.type.isCompatible(program, cell.expression.getType(program)))
                         conflicts.push(new Conflict(cell, SemanticConflict.TABLE_UPDATE_TYPE_MISMATCH));
                 }
             }
         });
+
+        // The query must be truthy.
+        if(this.query instanceof Expression && !(this.query.getType(program) instanceof BooleanType))
+            conflicts.push(new Conflict(this, SemanticConflict.TABLE_UPDATE_QUERY_MUST_BE_TRUTHY))
 
         return conflicts; 
     
