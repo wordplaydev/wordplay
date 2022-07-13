4fc466d5f53f7d8f3206ee2abf7dccc255d56079
Amy J. Ko <ajko@uw.edu>
2022-07-12 17:38:53 -0700

    Check that template expressions are text convertible.

--- src/parser/Template.ts
+++ src/parser/Template.ts
@@ -1,6 +1,7 @@
-import type Conflict from "./Conflict";
+import Conflict from "./Conflict";
 import Expression from "./Expression";
 import type Program from "./Program";
+import { SemanticConflict } from "./SemanticConflict";
 import TextType from "./TextType";
 import type { Token } from "./Token";
 import type Type from "./Type";
@@ -19,7 +20,20 @@ export default class Template extends Expression {
 
     getChildren() { return this.format ? [ ...this.parts, this.format ] : [ ...this.parts ]; }
 
-    getConflicts(program: Program): Conflict[] { return []; }
+    getConflicts(program: Program): Conflict[] { 
+        
+        const conflicts: Conflict[] = [];
+
+        // Expressions must be convertable to text.
+        (this.parts.filter(p => p instanceof Expression) as Expression[]).forEach(expr => {
+            const type = expr.getType(program);
+            if(type.getConversion(program, new TextType()) === undefined)
+                conflicts.push(new Conflict(expr, SemanticConflict.MISSING_CONVERSION));
+        });
+
+        return conflicts; 
+    
+    }
 
     getType(program: Program): Type {
         return new TextType(undefined, this.format);
