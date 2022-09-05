771c6f12bde37c169947796efcd05b4f76539725
Amy J. Ko <ajko@uw.edu>
2022-09-05 09:47:29 -0700

    Moved click handler to account for editor whitespace.

--- src/editor/Editor.svelte
+++ src/editor/Editor.svelte
@@ -17,11 +17,56 @@
         }
     }
 
+    function handleClick(event: MouseEvent) {
+
+        const el = event.currentTarget;
+
+        if(!(el instanceof HTMLElement)) return;
+
+        // First, ask for focus.
+        el.focus();
+
+        // Then, place the caret. Find the tokens that contain the vertical mouse position.
+        const tokenViews = el.querySelectorAll(".token-view");
+        const line: HTMLElement[] = [];
+        const mouseY = event.clientY;
+        const mouseX = event.clientX;
+        tokenViews.forEach(token => {
+            const tokenBounds = token.getBoundingClientRect();
+            const tokenTop = tokenBounds.top;
+            const tokenBottom = tokenBounds.bottom;
+            if(token instanceof HTMLElement && tokenTop <= mouseY && tokenBottom >= mouseY)
+                line.push(token);
+        });
+
+        // Of those aligned vertically, find the closest horizontally.
+        let closestDistance: number | undefined = undefined;
+        let closest: HTMLElement | undefined = undefined;
+        let left = false;
+        for(let i = 0; i < line.length; i++) {
+            const tokenBounds = line[i].getBoundingClientRect();
+            const tokenLeftDistance = Math.abs(tokenBounds.left - mouseX);
+            const tokenRightDistance = Math.abs(tokenBounds.right - mouseX);
+            const tokenDistance = Math.min(tokenLeftDistance, tokenRightDistance);
+            if(closestDistance === undefined || tokenDistance < closestDistance) {
+                closest = line[i];
+                closestDistance = tokenDistance;
+                left = tokenLeftDistance < tokenRightDistance;
+            }
+        };
+
+        if(closest !== undefined && closest.dataset.index !== undefined && closest.dataset.length !== undefined) {
+            caret.set($caret?.withPosition(parseInt(closest.dataset.index) + (left ? 0 : parseInt(closest.dataset.length))));
+        }
+
+    }
+
+
 </script>
 
 <div class="editor"
     tabindex=0
-    on:mousedown={(event) => event.currentTarget.focus()}
+    on:mousedown={handleClick}
     on:keydown={(event) => {
         if($project && $caret) {
             const meta = event.metaKey || event.ctrlKey;
--- src/editor/ProgramView.svelte
+++ src/editor/ProgramView.svelte
@@ -7,47 +7,8 @@
 
     export let program: Program;
 
-    function handleClick(event: MouseEvent) {
-        const el = event.currentTarget;
-        if(!(el instanceof HTMLElement)) return;
-
-        // Find the tokens that contain the vertical mouse position.
-        const tokenViews = el.querySelectorAll(".token-view");
-        const line: HTMLElement[] = [];
-        const mouseY = event.clientY;
-        const mouseX = event.clientX;
-        tokenViews.forEach(token => {
-            const tokenBounds = token.getBoundingClientRect();
-            const tokenTop = tokenBounds.top;
-            const tokenBottom = tokenBounds.bottom;
-            if(token instanceof HTMLElement && tokenTop <= mouseY && tokenBottom >= mouseY)
-                line.push(token);
-        });
-
-        // Of those aligned vertically, find the closest horizontally.
-        let closestDistance: number | undefined = undefined;
-        let closest: HTMLElement | undefined = undefined;
-        let left = false;
-        for(let i = 0; i < line.length; i++) {
-            const tokenBounds = line[i].getBoundingClientRect();
-            const tokenLeftDistance = Math.abs(tokenBounds.left - mouseX);
-            const tokenRightDistance = Math.abs(tokenBounds.right - mouseX);
-            const tokenDistance = Math.min(tokenLeftDistance, tokenRightDistance);
-            if(closestDistance === undefined || tokenDistance < closestDistance) {
-                closest = line[i];
-                closestDistance = tokenDistance;
-                left = tokenLeftDistance < tokenRightDistance;
-            }
-        };
-
-        if(closest !== undefined && closest.dataset.index !== undefined && closest.dataset.length !== undefined) {
-            caret.set($caret?.withPosition(parseInt(closest.dataset.index) + (left ? 0 : parseInt(closest.dataset.length))));
-        }
-
-    }
-
 </script>
 
-<NodeView node={program} block mousedown={handleClick}>
+<NodeView node={program} block>
     {#each program.borrows as borrow}<OptionalNodeView node={borrow}/>{/each}<OptionalNodeView node={program.block}/><TokenView node={program.end}/>
 </NodeView>
