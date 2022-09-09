261f17368fe9a246d5fe9e0889675fbc00914e01
Amy J. Ko <ajko@uw.edu>
2022-09-08 19:14:00 -0700

    Wrap optional nodes in node views.

--- src/editor/OptionalNodeView.svelte
+++ src/editor/OptionalNodeView.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import renderNode from "./renderNode";
     import type Node from "../nodes/Node";
+    import NodeView from "./NodeView.svelte";
 
     export let node: Node | undefined;
 </script>
 
-{#if node !== undefined}<svelte:component this={renderNode(node)} node={node} />{/if}
+{#if node !== undefined}<NodeView node={node}><svelte:component this={renderNode(node)} node={node} /></NodeView>{/if}
