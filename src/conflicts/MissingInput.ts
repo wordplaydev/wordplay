3a2378a14961f3533f5a3ca88415f03d474f8364
Amy J. Ko <ajko@uw.edu>
2022-09-12 11:33:28 -0700

    Missing input.

--- src/conflicts/MissingInput.ts
+++ src/conflicts/MissingInput.ts
@@ -9,10 +9,22 @@ export class MissingInput extends Conflict {
     readonly func: FunctionType | StructureType;
     readonly evaluate: Evaluate;
     readonly input: Input;
+
     constructor(func: FunctionType | StructureType, evaluate: Evaluate, input: Input) {
         super(false);
         this.func = func;
         this.evaluate = evaluate;
         this.input = input;
     }
+
+    getConflictingNodes() {
+        return [ ...this.input.aliases, this.evaluate ];
+    }
+
+    getExplanations() { 
+        return {
+            eng: `Expected an input ${this.input.aliases.map(a => a.getName()).join(", ")}, but it wasn't provided.`
+        }
+    }
+
 }
