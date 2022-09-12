13106f5d2b6369b56028cdc876f5cb1348d83a7a
Amy J. Ko <ajko@uw.edu>
2022-09-12 13:35:51 -0700

    Placeholder conflict messages.

--- src/conflicts/Placeholder.ts
+++ src/conflicts/Placeholder.ts
@@ -4,9 +4,22 @@ import Conflict from "./Conflict";
 
 
 export class Placeholder extends Conflict {
+
     readonly placeholder: ExpressionPlaceholder | TypePlaceholder;
+
     constructor(placeholder: ExpressionPlaceholder | TypePlaceholder) {
         super(true);
         this.placeholder = placeholder;
     }
+
+    getConflictingNodes() {
+        return [ this.placeholder ];
+    }
+
+    getExplanations() { 
+        return {
+            eng: `Don't forget to replace his with some code!`
+        }
+    }
+
 }
