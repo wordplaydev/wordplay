2e51b79a001177226d0953b2d91c793bdb689938
Amy J. Ko <ajko@uw.edu>
2022-09-12 13:56:49 -0700

    Unnamed alias conflict message.

--- src/conflicts/UnnamedAlias.ts
+++ src/conflicts/UnnamedAlias.ts
@@ -5,7 +5,18 @@ import Conflict from "./Conflict";
 export default class UnnamedAlias extends Conflict {
     readonly alias: Alias;
     constructor(alias: Alias) {
-        super(false);
+        super(true);
         this.alias = alias;
     }
+
+    getConflictingNodes() {
+        return [ this.alias ];
+    }
+
+    getExplanations() { 
+        return {
+            eng: `There should a name after me`
+        }
+    }
+
 }
