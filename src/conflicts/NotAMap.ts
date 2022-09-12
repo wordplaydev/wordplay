cec40aec991d7f1f9c950a0dcdc714f4f51c95a3
Amy J. Ko <ajko@uw.edu>
2022-09-12 11:45:49 -0700

    Not a map messages.

--- src/conflicts/NotAMap.ts
+++ src/conflicts/NotAMap.ts
@@ -1,11 +1,24 @@
+import Expression from "../nodes/Expression";
 import type MapLiteral from "../nodes/MapLiteral";
 import Conflict from "./Conflict";
 
 
 export class NotASetOrMap extends Conflict {
     readonly map: MapLiteral;
+
     constructor(set: MapLiteral) {
         super(false);
         this.map = set;
     }
+
+    getConflictingNodes() {
+        return this.map.values.filter(n => n instanceof Expression);
+    }
+
+    getExplanations() { 
+        return {
+            eng: `These values aren't key:value pairs, but others are, so this is an invalid map literal.`
+        }
+    }
+
 }
