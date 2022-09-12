578122abe929e5c39a40c676f85f8cc10d81f33a
Amy J. Ko <ajko@uw.edu>
2022-09-12 11:49:33 -0700

    Not a number message.

--- src/conflicts/NotANumber.ts
+++ src/conflicts/NotANumber.ts
@@ -4,8 +4,19 @@ import Conflict from "./Conflict";
 
 export class NotANumber extends Conflict {
     readonly measurement: MeasurementLiteral;
+
     constructor(measurement: MeasurementLiteral) {
         super(false);
         this.measurement = measurement;
+    }
+
+    getConflictingNodes() {
+        return [ this.measurement ];
+    }
+
+    getExplanations() { 
+        return {
+            eng: `This number isn't formatted correctly`
+        }
     }
 }
