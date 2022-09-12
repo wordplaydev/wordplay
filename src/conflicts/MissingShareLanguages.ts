89cdb113dd483ee0bd67b82f629a875ff22c3f68
Amy J. Ko <ajko@uw.edu>
2022-09-12 11:37:14 -0700

    Missing language conflict.

--- src/conflicts/MissingShareLanguages.ts
+++ src/conflicts/MissingShareLanguages.ts
@@ -4,8 +4,20 @@ import Conflict from "./Conflict";
 
 export class MissingShareLanguages extends Conflict {
     readonly share: Share;
+
     constructor(share: Share) {
         super(false);
         this.share = share;
     }
+
+    getConflictingNodes() {
+        return [ this.share ];
+    }
+
+    getExplanations() { 
+        return {
+            eng: `To share something, you must tag it's names with languages.`
+        }
+    }
+
 }
