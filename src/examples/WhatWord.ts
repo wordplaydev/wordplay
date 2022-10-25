cbf8f93c66e9fc0f7a0927ac68a32c41ed1e4734
Amy J. Ko <ajko@uw.edu>
2022-10-24 21:40:19 -0400

    Added emoji aliases to what word for testing.

--- src/examples/WhatWord.ts
+++ src/examples/WhatWord.ts
@@ -5,21 +5,22 @@ const WhatWord = {
 ↓ words
 \`The game state is a list of guesses and a secret word.\`/eng
 \`El estado del juego es una lista de conjeturas y una palabra secreta.\`/spa
-•Game(guesses•[""] secret•"")
+\`⚽️ = 🤔 + 🤫\`/😀
+•Game(guesses/eng,🤔/😀•[""] secret/eng,🤫/😀•"")
 (
-\tƒ guessesRemaining() (secret.length() · 2) - guesses.length()
-\tƒ status()
+\tƒ guessesRemaining/eng,🔢/😀() (secret.length() · 2) - guesses.length()
+\tƒ status/eng,🚨/😀()
 \t\tsecret = "" ? "start"
 \t\tguessesRemaining() ≤ 0 ? "lost"
 \t\tsecret→[""].all(ƒ(letter•"") guesses.has(letter)) ? "won"
 \t\t"playing"
 )
 
-start: Game([] "")
+start/eng,🟢/😀: Game([] "")
 
 \`When the keyboard state changes, the game state changes.\`/eng
 \`Cuando cambia el estado del teclado, cambia el estado del juego.\`/spa
-state•Game: start ∆ ⌨️
+state/eng,⚽️/😀•Game: start ∆ ⌨️
 \t\t⌨️.key = "Escape" ? start
 \t\t(state.status() ≠ "playing") ∧ (⌨️.key = " ") ∧ ⌨️.down ? Game([] words.random())
 \t\t(state.status() = "playing") ∧ ⌨️.down ∧ (⌨️.key.length() = 1) ∧ ¬state.guesses.has(⌨️.key) ? Game(state.guesses.add(⌨️.key) state.secret)
@@ -27,11 +28,11 @@ state•Game: start ∆ ⌨️
 
 \`The board is a translation of the secret, accounting for guesses\`/eng
 \`El tablero es una traducción del secreto, teniendo en cuenta las conjeturas.\`/spa
-board: Phrase(state.secret→[""].translate(ƒ(letter•"") ((state.status() = "lost") ∨ state.guesses.has(letter)) ? letter "_").join(' ') 24pt)
+board/eng,🔤/😀: Phrase(state.secret→[""].translate(ƒ(letter•"") ((state.status() = "lost") ∨ state.guesses.has(letter)) ? letter "_").join(' ') 24pt)
 
 \`The rest of the game screen depends on the game status\`/eng
 \`El resto de la pantalla del juego depende del estado del juego.\`/spa
-screen:
+screen/eng,📺/😀:
 \tstate.status() = "start" ? Group(Vertical() Phrase("Welcome to WhatWord!" 30pt) Phrase("Press space to begin") Phrase("Type letters to guess"))
 \tstate.status() = "lost" ?  Group(Vertical() board Phrase("You lost. Press space to play again."))
 \tstate.status() = "won" ?   Group(Vertical() board Phrase("You won, nice job! Press space to play again."))
