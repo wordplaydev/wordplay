fb0313bea0d9a77ec641d653a861afb8c50039c8
Amy J. Ko <ajko@uw.edu>
2022-11-14 09:41:43 -0800

    Better testing of examples.

--- src/examples/examples.test.ts
+++ src/examples/examples.test.ts
@@ -4,8 +4,7 @@ import { examples, makeProject } from "./examples";
 for(const example of examples) {
     test(`Ensure "${example.name}" has no conflicts`, () => {
         const project = makeProject(example);
-        expect(project.main.conflicts).toHaveLength(0);
-        for(const source of project.supplements)
+        for(const source of project.getSources())
             expect(source.conflicts).toHaveLength(0);
     });
 }
