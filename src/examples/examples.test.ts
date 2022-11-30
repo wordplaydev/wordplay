508440a306038787b968a5a2d51ea2e509aa7e47
Amy J. Ko <ajko@uw.edu>
2022-11-30 07:56:28 -0800

    Fixed test.

--- src/examples/examples.test.ts
+++ src/examples/examples.test.ts
@@ -4,7 +4,6 @@ import { examples, makeProject } from "./examples";
 for(const example of examples) {
     test(`Ensure "${example.name}" has no conflicts`, () => {
         const project = makeProject(example);
-        for(const source of project.getSources())
-            expect(source._conflicts).toHaveLength(0);
+        expect(project.primaryConflicts).toHaveLength(0);
     });
 }
