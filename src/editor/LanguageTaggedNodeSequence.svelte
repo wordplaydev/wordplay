9a871da374d4a426864307cabb3611f948f9ff64
Amy J. Ko <ajko@uw.edu>
2022-12-23 17:33:09 -0800

    Only hide languages in Program roots.

--- src/editor/LanguageTaggedNodeSequence.svelte
+++ src/editor/LanguageTaggedNodeSequence.svelte
@@ -1,6 +1,6 @@
 <svelte:options immutable={true}/>
 <script lang="ts">
-    import { getCaret } from "./util/Contexts";
+    import { getCaret, getRoot } from "./util/Contexts";
     import { languages } from "../models/languages";
     import type Node from "../nodes/Node";
     import type Doc from "../nodes/Doc";
@@ -8,18 +8,20 @@
     import NodeView from "./NodeView.svelte";
     import type LanguageCode from "../nodes/LanguageCode";
     import { project } from "../models/stores";
+    import Program from "../nodes/Program";
 
     export let node: Node;
     export let list: (Doc|Name)[];
 
+    let root = getRoot();
     let caret = getCaret();
-    $: inside = $project.evaluator.isPlaying() && $caret?.isIn(node);
+    $: show = !($root?.node instanceof Program) || ($project.evaluator.isPlaying() && $caret?.isIn(node));
     $: visible = list.filter(item => $languages.includes((item.lang?.getLanguage() ?? "") as LanguageCode));
 
 </script>
 
 <!-- If the caret is in the docs, show all of them for navigation and editing purposes. Otherwise, just show the selected languages -->
-{#each list as item, index }{@const lang = item.lang?.getLanguageCode() }<span class={`language-tagged ${(visible.length === 0 && index === 0) || (lang && $languages.includes(lang)) ? "visible" : inside ? "inside" : "hidden"} ${(visible.length > 0 && visible[0] === item) || (visible.length === 0 && index === 0) ? "first-visible" : ""} ${index === 0 ? "first" : ""}`}><NodeView node={item} /></span>{/each}
+{#each list as item, index }{@const lang = item.lang?.getLanguageCode() }<span class={`language-tagged ${(visible.length === 0 && index === 0) || (lang && $languages.includes(lang)) ? "visible" : show ? "inside" : "hidden"} ${(visible.length > 0 && visible[0] === item) || (visible.length === 0 && index === 0) ? "first-visible" : ""} ${index === 0 ? "first" : ""}`}><NodeView node={item} /></span>{/each}
 
 <style>
     .language-tagged :global(.text) {
--- src/editor/LanguageView.svelte
+++ src/editor/LanguageView.svelte
@@ -1,16 +1,18 @@
 <svelte:options immutable={true}/>
 <script lang="ts">
     import type Language from "../nodes/Language";
-    import { getCaret } from "./util/Contexts";
+    import { getCaret, getRoot } from "./util/Contexts";
     import NodeView from "./NodeView.svelte";
     import { project } from "../models/stores";
+    import Program from "../nodes/Program";
     
     export let node: Language;
 
+    let root = getRoot();
     let caret = getCaret();
     $: parent = $caret?.source.get(node)?.getParent();
-    $: inside = parent && $caret && $caret.isIn(parent)
+    $: show = !($root?.node instanceof Program) || (parent && $caret && $caret.isIn(parent))
 
 </script>
 
-{#if inside && $project.evaluator.isPlaying()}<em><NodeView node={node.slash}/><NodeView node={node.lang}/></em>{/if}
+{#if show && $project.evaluator.isPlaying()}<em><NodeView node={node.slash}/><NodeView node={node.lang}/></em>{/if}
