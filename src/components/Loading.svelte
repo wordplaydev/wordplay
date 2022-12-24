a8710beffca44d4230f68e0c9649a7cb44c4b461
Amy J. Ko <ajko@uw.edu>
2022-12-23 20:16:50 -0800

    Prettier loading.

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,9 +1,20 @@
-<div>Loading...</div>
+<div>|</div>
 
 <style>
     div {
-        width: 100vw;
-        height: 100vw;
-        margin: auto;
+        font-family: "Courier New", monospace;
+        font-size: 72pt;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        animation: wobble 0.5s infinite linear;
     }
+
+    @keyframes wobble {
+        0% { transform: rotate(0deg); }
+        50% { transform: rotate(180deg); }
+        100% { transform: rotate(360deg); }
+    }
+
 </style>
