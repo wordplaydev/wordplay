2d492cff51a18f1187ea3f208d57eed924c27350
Amy J. Ko <ajko@uw.edu>
2023-01-02 08:07:15 -0800

    Set test refinement.

--- src/native/SetNative.test.ts
+++ src/native/SetNative.test.ts
@@ -1,26 +1,14 @@
 import { test, expect } from 'vitest';
 import Evaluator from '../runtime/Evaluator';
 
-test('Test set functions', () => {
-    expect(Evaluator.evaluateCode('{1 2 3}.add(1)')?.toString()).toBe(
-        '{1 2 3}'
-    );
-    expect(Evaluator.evaluateCode('{1 2 3}.add(4)')?.toString()).toBe(
-        '{1 2 3 4}'
-    );
-    expect(Evaluator.evaluateCode('{1 2 3}.remove(1)')?.toString()).toBe(
-        '{2 3}'
-    );
-    expect(Evaluator.evaluateCode('{1 2 3}.union({3 4})')?.toString()).toBe(
-        '{1 2 3 4}'
-    );
-    expect(
-        Evaluator.evaluateCode('{1 2 3}.intersection({2 3 4})')?.toString()
-    ).toBe('{2 3}');
-    expect(
-        Evaluator.evaluateCode('{1 2 3}.difference({3 4 5})')?.toString()
-    ).toBe('{1 2}');
-    expect(
-        Evaluator.evaluateCode('{1 2 3}.filter(ƒ(v) v % 2 = 1)')?.toString()
-    ).toBe('{1 3}');
+test.each([
+    ['{1 2 3}.add(1)', '{1 2 3}'],
+    ['{1 2 3}.add(4)', '{1 2 3 4}'],
+    ['{1 2 3}.remove(1)', '{2 3}'],
+    ['{1 2 3}.union({3 4})', '{1 2 3 4}'],
+    ['{1 2 3}.intersection({2 3 4})', '{2 3}'],
+    ['{1 2 3}.difference({3 4 5})', '{1 2}'],
+    ['{1 2 3}.filter(ƒ(v) v % 2 = 1)', '{1 3}'],
+])('Test set functions', (code, value) => {
+    expect(Evaluator.evaluateCode(code)?.toString()).toBe(value);
 });
