d887f3640bc8ecd29ff07314b6165d142832bb31
Amy J. Ko <ajko@uw.edu>
2023-01-02 08:13:26 -0800

    Improved bind tests.

--- src/nodes/Bind.test.ts
+++ src/nodes/Bind.test.ts
@@ -9,21 +9,25 @@ import Names from './Names';
 import { MisplacedShare } from '../conflicts/MisplacedShare';
 import { MissingShareLanguages } from '../conflicts/MissingShareLanguages';
 
-test('Test bind conflicts', () => {
-    testConflict('a, b: 1\na', 'a, a: 1\na', Names, DuplicateNames);
-    testConflict('a•#: 1\na', 'a•"": 1\na', Bind, IncompatibleBind);
-    testConflict(
-        'a•#: 1\na',
-        'a•"cat"|"dot": "mouse"\na',
-        Bind,
-        IncompatibleBind
-    );
-    testConflict('a•#: 1\na', 'a•1|2: 3\na', Bind, IncompatibleBind);
-    testConflict('a: 1\na+a', 'a: 1\n1+1', Bind, UnusedBind);
-    testConflict('↑ a: 1', 'ƒ() (↑ a: 1)', Bind, MisplacedShare);
-    testConflict('↑ a/eng: 1', '↑ a: 1', Bind, MissingShareLanguages);
-});
+test.each([
+    ['a, b: 1\na', 'a, a: 1\na', Names, DuplicateNames],
+    ['a•#: 1\na', 'a•"": 1\na', Bind, IncompatibleBind],
+    ['a•#: 1\na', 'a•"cat"|"dot": "mouse"\na', Bind, IncompatibleBind],
+    ['a•#: 1\na', 'a•1|2: 3\na', Bind, IncompatibleBind],
+    ['a: 1\na+a', 'a: 1\n1+1', Bind, UnusedBind],
+    ['↑ a: 1', 'ƒ() (↑ a: 1)', Bind, MisplacedShare],
+    ['↑ a/eng: 1', '↑ a: 1', Bind, MissingShareLanguages],
+])(
+    'Expect %s no conflicts, %s to have %s with %s',
+    (good, bad, node, conflict, number?) => {
+        testConflict(good, bad, node, conflict, number);
+    }
+);
 
 test('Test binding logic', () => {
     expect(Evaluator.evaluateCode('a: 5\na')?.toString()).toBe('5');
+});
+
+test.each([['a: 5\na', '5']])('Expect %s to be %s', (code, value) => {
+    expect(Evaluator.evaluateCode(code)?.toString()).toBe(value);
 });
