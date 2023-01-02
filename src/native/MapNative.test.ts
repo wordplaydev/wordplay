c728fdb4dd1024b8cbed44e438946c4425e73eaa
Amy J. Ko <ajko@uw.edu>
2023-01-02 08:05:27 -0800

    Better map tests.

--- src/native/MapNative.test.ts
+++ src/native/MapNative.test.ts
@@ -1,27 +1,16 @@
 import { test, expect } from 'vitest';
 import Evaluator from '../runtime/Evaluator';
 
-test('Test map functions', () => {
-    expect(
-        Evaluator.evaluateCode('{1:"hi" 2:"bye"}.set(3 "hello")')?.toString()
-    ).toBe('{1:"hi" 2:"bye" 3:"hello"}');
-    expect(
-        Evaluator.evaluateCode('{1:"hi" 2:"bye"}.set(1 "hello")')?.toString()
-    ).toBe('{1:"hello" 2:"bye"}');
-    expect(
-        Evaluator.evaluateCode('{1:"hi" 2:"bye"}.unset(1)')?.toString()
-    ).toBe('{2:"bye"}');
-    expect(
-        Evaluator.evaluateCode('{1:"hi" 2:"bye"}.remove("bye")')?.toString()
-    ).toBe('{1:"hi"}');
-    expect(
-        Evaluator.evaluateCode(
-            "{'cat':1 'dog':2 'mouse':3}.filter(ƒ(k v) v ≥ 3)"
-        )?.toString()
-    ).toBe('{"mouse":3}');
-    expect(
-        Evaluator.evaluateCode(
-            "{'cat':1 'dog':2 'mouse':3}.translate(ƒ(k v) -v)"
-        )?.toString()
-    ).toBe('{"cat":-1 "dog":-2 "mouse":-3}');
+test.each([
+    ['{1:"hi" 2:"bye"}.set(3 "hello")', '{1:"hi" 2:"bye" 3:"hello"}'],
+    ['{1:"hi" 2:"bye"}.set(1 "hello")', '{1:"hello" 2:"bye"}'],
+    ['{1:"hi" 2:"bye"}.unset(1)', '{2:"bye"}'],
+    ['{1:"hi" 2:"bye"}.remove("bye")', '{1:"hi"}'],
+    ["{'cat':1 'dog':2 'mouse':3}.filter(ƒ(k v) v ≥ 3)", '{"mouse":3}'],
+    [
+        "{'cat':1 'dog':2 'mouse':3}.translate(ƒ(k v) -v)",
+        '{"cat":-1 "dog":-2 "mouse":-3}',
+    ],
+])('Test map functions', (code, value) => {
+    expect(Evaluator.evaluateCode(code)?.toString()).toBe(value);
 });
