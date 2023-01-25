b3d93e65bfd142851aed9dd3b02452a36715abe7
Amy J. Ko <ajko@uw.edu>
2023-01-25 12:44:00 -0800

    Collapsable list view.

--- src/components/values/ListView.svelte
+++ src/components/values/ListView.svelte
@@ -6,16 +6,19 @@
     import SymbolView from './SymbolView.svelte';
     import ValueView from './ValueView.svelte';
     import TokenType from '@nodes/TokenType';
+    import Expandable from './Expandable.svelte';
 
     export let value: List;
 </script>
 
-<SymbolView
-    symbol={LIST_OPEN_SYMBOL}
-    type={TokenType.LIST_OPEN}
-/>{#each value.values as item, index}<ValueView
-        value={item}
-    />{#if index < value.values.length - 1}{' '}{/if}{/each}<SymbolView
-    symbol={LIST_CLOSE_SYMBOL}
-    type={TokenType.LIST_CLOSE}
-/>
+<SymbolView symbol={LIST_OPEN_SYMBOL} type={TokenType.LIST_OPEN} /><Expandable
+    ><svelte:fragment slot="expanded"
+        >{#each value.values as item, index}<ValueView
+                value={item}
+            />{#if index < value.values.length - 1}{' '}{/if}{/each}</svelte:fragment
+    ><svelte:fragment slot="collapsed"
+        >{#each value.values.slice(0, 3) as item, index}<ValueView
+                value={item}
+            />{#if index < value.values.length - 1}{' '}{/if}{/each}{#if value.values.length > 3}…{/if}</svelte:fragment
+    ></Expandable
+><SymbolView symbol={LIST_CLOSE_SYMBOL} type={TokenType.LIST_CLOSE} />
