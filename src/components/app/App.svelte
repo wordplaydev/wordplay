c1c59b9f356ca729b0c894324167ac9dfc756d20
Amy J. Ko <ajko@uw.edu>
2023-03-01 08:51:01 -0800

    Firebase configuration.

--- src/components/app/App.svelte
+++ src/components/app/App.svelte
@@ -6,6 +6,9 @@
     import Loading from './Loading.svelte';
     import ProjectChooser from './ProjectChooser.svelte';
     import { ProjectSymbol, type ProjectContext } from '../project/Contexts';
+    import { PUBLIC_CONTEXT } from '$env/static/public';
+
+    if (PUBLIC_CONTEXT !== 'prod') console.log(`*** ${PUBLIC_CONTEXT} ***`);
 
     // Don't display the manager until the fonts are loaded.
     let fontsLoaded = false;
@@ -19,7 +22,12 @@
 
 <main class:animated={$animationsOn}>
     {#if fontsLoaded}
-        {#if $project}
+        {#if PUBLIC_CONTEXT === 'prod'}
+            <div class="teaser">
+                <h1><strong>Wordplay.dev</strong> is coming</h1>
+                <p>Curious? Write <a href="https://amyjko.com">Amy</a></p>
+            </div>
+        {:else if $project}
             <ProjectView project={$project} />
         {:else}
             <ProjectChooser />
@@ -28,3 +36,14 @@
         <Loading />
     {/if}
 </main>
+
+<style>
+    .teaser {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/app/firebase.ts
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
+import { getAnalytics } from 'firebase/analytics';
+import {
+    PUBLIC_CONTEXT,
+    PUBLIC_FIREBASE_API_KEY,
+    PUBLIC_FIREBASE_AUTH_DOMAIN,
+    PUBLIC_FIREBASE_PROJECT_ID,
+    PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    PUBLIC_FIREBASE_APP_ID,
+} from '$env/static/public';
+
+const firebaseConfig = {
+    apiKey: PUBLIC_FIREBASE_API_KEY,
+    authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: PUBLIC_FIREBASE_PROJECT_ID,
+    messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const functions = getFunctions(app);
+
+// Initialize emulator
+if (PUBLIC_CONTEXT === 'local') {
+    connectFirestoreEmulator(firestore, 'localhost', 8080);
+    connectAuthEmulator(auth, 'http://localhost:9099');
+    connectFunctionsEmulator(functions, 'localhost', 5001);
+}
