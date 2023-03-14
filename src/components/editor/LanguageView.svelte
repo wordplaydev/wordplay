0af532dfd9f089ac26093d6ad5d3d184861c0172
Amy J. Ko <ajko@uw.edu>
2023-03-14 10:38:00 -0700

    Fixed language view visiblity on stepping.

--- src/components/editor/LanguageView.svelte
+++ src/components/editor/LanguageView.svelte
@@ -2,17 +2,15 @@
 
 <script lang="ts">
     import type Language from '@nodes/Language';
-    import { getCaret, getEvaluator } from '../project/Contexts';
+    import { getCaret } from '../project/Contexts';
     import NodeView from './NodeView.svelte';
 
     export let node: Language;
 
-    let evaluator = getEvaluator();
     let caret = getCaret();
     $: parent = $caret?.source.root.getParent(node);
     $: show = $caret === undefined || (parent && $caret.isIn(parent));
 </script>
 
-{#if show && $evaluator?.isPlaying()}<em
-        ><NodeView node={node.slash} /><NodeView node={node.lang} /></em
+{#if show}<em><NodeView node={node.slash} /><NodeView node={node.lang} /></em
     >{/if}
