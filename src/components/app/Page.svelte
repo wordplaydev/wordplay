a86079ef88c8bd85fe7e3b77c3c02ac5657fbfe5
Amy J. Ko <ajko@uw.edu>
2023-03-23 15:38:42 -0700

    Fixed page layout to enable scrolling.

--- src/components/app/Page.svelte
+++ src/components/app/Page.svelte
@@ -15,11 +15,7 @@
 
 <style>
     .page {
-        position: absolute;
-        left: 0;
-        top: 0;
-        right: 0;
-        bottom: 0;
+        width: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
