435f859b6aa43b503fb1a6469fe0469d193a2af4
Amy J. Ko <ajko@uw.edu>
2023-03-28 08:14:13 -0700

    Fixed duplicate.

--- src/examples/examples.ts
+++ src/examples/examples.ts
@@ -19,7 +19,7 @@ import Timer from './Timer.wp?raw';
 import Poem from './Poem.wp?raw';
 import Garden from './Garden.wp?raw';
 import Cannon from './Cannon.wp?raw';
-import Between from './BetweenConflict.wp?raw';
+import Between from './Between.wp?raw';
 import { parseNames, toTokens } from '../parser/Parser';
 import type Names from '../nodes/Names';
 
