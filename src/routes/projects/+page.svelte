25d46bcb223cf97a257bd6bb07f98ea4dd766016
Amy J. Ko <ajko@uw.edu>
2023-03-30 15:31:53 -0700

    Added title to projects page.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,12 @@
 <script>
     import ProjectChooser from '@components/app/ProjectChooser.svelte';
     import Page from '@components/app/Page.svelte';
+    import { preferredTranslations } from '@translation/translations';
 </script>
+
+<svelte:head>
+    <title>{$preferredTranslations[0].ui.headers.projects}</title>
+</svelte:head>
 
 <Page>
     <ProjectChooser />
