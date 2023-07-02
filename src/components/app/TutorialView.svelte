205fb82d7d92b8d2c9f7740aa5fff610fdede41c
Amy J. Ko <ajko@uw.edu>
2023-07-02 11:57:57 -0700

    Pop in turns in tutorial dialog.

--- src/components/app/TutorialView.svelte
+++ src/components/app/TutorialView.svelte
@@ -179,22 +179,36 @@
                     >{#if scene.concept}<em>{scene.concept}</em>{/if}</div
                 >
             {:else}
-                {#each turns as turn}
-                    <!-- First speaker is always function, alternating speakers are the concept we're learning about. -->
-                    <Speech
-                        glyph={$conceptsStore
-                            ?.getConceptByName(turn.dialog.concept)
-                            ?.getGlyphs($creator.getLanguages()) ?? {
-                            symbols: turn.dialog.concept,
-                        }}
-                        right={turn.dialog.concept === 'FunctionDefinition'}
-                        baseline
-                        scroll={false}
-                        emotion={turn.dialog.emotion}
-                    >
-                        <DocHtmlView doc={turn.speech} spaces={turn.spaces} />
-                    </Speech>
-                {/each}
+                {#key turns}
+                    {#each turns as turn, index}
+                        <!-- First speaker is always function, alternating speakers are the concept we're learning about. -->
+                        <div
+                            class="turn"
+                            class:animated={$creator.getAnimationFactor() > 0}
+                            style="--delay:{$creator.getAnimationDuration() *
+                                2 *
+                                index}ms"
+                        >
+                            <Speech
+                                glyph={$conceptsStore
+                                    ?.getConceptByName(turn.dialog.concept)
+                                    ?.getGlyphs($creator.getLanguages()) ?? {
+                                    symbols: turn.dialog.concept,
+                                }}
+                                right={turn.dialog.concept ===
+                                    'FunctionDefinition'}
+                                baseline
+                                scroll={false}
+                                emotion={turn.dialog.emotion}
+                            >
+                                <DocHtmlView
+                                    doc={turn.speech}
+                                    spaces={turn.spaces}
+                                />
+                            </Speech>
+                        </div>
+                    {/each}
+                {/key}
             {/if}
         </div>
         <nav>
@@ -328,6 +342,35 @@
 
     .dialog:focus {
         outline-offset: calc(-1 * var(--wordplay-focus-width));
+    }
+
+    .turn.animated {
+        opacity: 0;
+        animation-name: popIn;
+        animation-duration: 0.5s;
+        animation-delay: var(--delay);
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes popIn {
+        0% {
+            opacity: 0;
+            transform: scale(0);
+        }
+        25% {
+            transform: scaleY(0.25);
+        }
+        50% {
+            transform: scaleY(0.75);
+        }
+        80% {
+            opacity: 0;
+            transform: scale(1.1);
+        }
+        100% {
+            opacity: 1;
+            transform: scale(1);
+        }
     }
 
     nav {
