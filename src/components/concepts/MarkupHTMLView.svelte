af2a198e6a015cbe5f030c61c4d9743fa945abe7
Amy J. Ko <ajko@uw.edu>
2023-07-12 08:32:15 -0700

    Reuse of segment logic.

--- src/components/concepts/MarkupHTMLView.svelte
+++ src/components/concepts/MarkupHTMLView.svelte
@@ -1,22 +1,9 @@
 <svelte:options immutable />
 
 <script lang="ts">
-    import WebLink from '@nodes/WebLink';
-    import Words from '@nodes/Words';
-    import ConceptLink from '@nodes/ConceptLink';
-    import ConceptLinkUI from './ConceptLinkUI.svelte';
-    import Example from '@nodes/Example';
-    import ExampleUI from './ExampleUI.svelte';
     import { creator } from '../../db/Creator';
-    import WebLinkHTMLView from './WebLinkHTMLView.svelte';
-    import WordsHTMLView from './WordsHTMLView.svelte';
     import type Markup from '../../nodes/Markup';
-    import Token from '../../nodes/Token';
-    import NodeRef from '../../locale/NodeRef';
-    import ValueRef from '../../locale/ValueRef';
-    import ValueView from '../values/ValueView.svelte';
-    import ConceptRef from '../../locale/ConceptRef';
-    import NodeView from '../editor/NodeView.svelte';
+    import SegmentHTMLView from './SegmentHTMLView.svelte';
 
     export let markup: Markup;
 
@@ -29,27 +16,7 @@
             class:animated={$creator.getAnimationFactor() > 0}
             style="--delay:{$creator.getAnimationDuration() * index * 0.5}ms"
             >{#each paragraph.segments as segment}
-                {#if segment instanceof WebLink}<WebLinkHTMLView
-                        link={segment}
-                        {spaces}
-                    />{:else if segment instanceof Example}<ExampleUI
-                        example={segment}
-                        {spaces}
-                        evaluated={paragraph.segments.length === 1}
-                        inline={paragraph.segments.length > 1}
-                    />{:else if segment instanceof ConceptLink}<ConceptLinkUI
-                        link={segment}
-                    />{:else if segment instanceof Words}<WordsHTMLView
-                        words={segment}
-                        {spaces}
-                    />{:else if segment instanceof NodeRef}<NodeView
-                        node={segment.node}
-                    />{:else if segment instanceof ValueRef}<strong
-                        ><ValueView value={segment.value} /></strong
-                    >{:else if segment instanceof ConceptRef}<ConceptLinkUI
-                        link={segment}
-                    />{:else if segment instanceof Token}{segment.getText()}
-                {/if}
+                <SegmentHTMLView {segment} {spaces} />
             {/each}</p
         >{/each}
 {:else}
---
+++ src/components/concepts/SegmentHTMLView.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import ConceptLink from '../../nodes/ConceptLink';
+    import Token from '../../nodes/Token';
+    import WebLink from '../../nodes/WebLink';
+    import Words from '../../nodes/Words';
+    import type Spaces from '../../parser/Spaces';
+    import WebLinkHTMLView from './WebLinkHTMLView.svelte';
+    import ConceptLinkUI from './ConceptLinkUI.svelte';
+    import Example from '../../nodes/Example';
+    import ExampleUI from './ExampleUI.svelte';
+    import NodeRef from '../../locale/NodeRef';
+    import NodeView from '../editor/NodeView.svelte';
+    import ValueRef from '../../locale/ValueRef';
+    import ValueView from '../values/ValueView.svelte';
+    import ConceptRef from '../../locale/ConceptRef';
+    import type { Segment } from '../../nodes/Paragraph';
+    import WordsHTMLView from './WordsHTMLView.svelte';
+
+    export let segment: Segment;
+    export let spaces: Spaces;
+</script>
+
+{#if segment instanceof WebLink}<WebLinkHTMLView
+        link={segment}
+        {spaces}
+    />{:else if segment instanceof Example}<ExampleUI
+        example={segment}
+        {spaces}
+        evaluated={false}
+        inline={true}
+    />{:else if segment instanceof ConceptLink}<ConceptLinkUI
+        link={segment}
+    />{:else if segment instanceof Words}<WordsHTMLView
+        words={segment}
+        {spaces}
+    />{:else if segment instanceof NodeRef}<NodeView
+        node={segment.node}
+    />{:else if segment instanceof ValueRef}<strong
+        ><ValueView value={segment.value} /></strong
+    >{:else if segment instanceof ConceptRef}<ConceptLinkUI
+        link={segment}
+    />{:else if segment instanceof Token}{#if spaces.getSpace(segment).length > 0}&nbsp;{/if}{segment.getText()}{/if}
