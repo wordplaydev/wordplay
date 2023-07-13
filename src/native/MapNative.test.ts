54120f5c36c1654b6ec051aa630a75746bd0ab88
Amy J. Ko <ajko@uw.edu>
2023-07-13 11:18:23 -0700

    Map docs.

--- src/native/MapNative.test.ts
+++ src/native/MapNative.test.ts
@@ -5,9 +5,9 @@ import { getDefaultNative } from './Native';
 const native = await getDefaultNative();
 
 test.each([
-    ['{1:"hi" 2:"bye"}.set(3 "hello")', '{1:"hi" 2:"bye" 3:"hello"}'],
-    ['{1:"hi" 2:"bye"}.set(1 "hello")', '{1:"hello" 2:"bye"}'],
-    ['{1:"hi" 2:"bye"}.unset(1)', '{2:"bye"}'],
+    ['{1:"hi" 2:"bye"}.pair(3 "hello")', '{1:"hi" 2:"bye" 3:"hello"}'],
+    ['{1:"hi" 2:"bye"}.pair(1 "hello")', '{1:"hello" 2:"bye"}'],
+    ['{1:"hi" 2:"bye"}.unpair(1)', '{2:"bye"}'],
     ['{1:"hi" 2:"bye"}.remove("bye")', '{1:"hi"}'],
     ["{'cat':1 'dog':2 'mouse':3}.filter(ƒ(k v) v ≥ 3)", '{"mouse":3}'],
     [
