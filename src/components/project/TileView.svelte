714ee47535c0364c4f5f496d2e85dacf6af8a64f
Amy J. Ko <ajko@uw.edu>
2023-07-25 16:32:51 -0700

    Scrollable tile toolbar.

--- src/components/project/TileView.svelte
+++ src/components/project/TileView.svelte
@@ -190,7 +190,7 @@
         bind:this={view}
     >
         <!-- Render the toolbar -->
-        <div class="controls">
+        <div class="header">
             <div class="name" class:source={tile.isSource()}>
                 {#if tile.isSource()}
                     {Glyphs.Program.symbols}
@@ -208,34 +208,37 @@
                 {/if}
                 <slot name="name" />
             </div>
-            <slot name="extra" />
-            <Button
-                tip={$config.getLocale().ui.description.collapse}
-                action={() => dispatch('mode', { mode: Mode.Collapsed })}
-                active={!layout.isFullscreen()}>⎵</Button
-            >
-            <Button
-                tip={$config.getLocale().ui.description.fullscreen}
-                action={() =>
-                    dispatch('fullscreen', {
-                        fullscreen: !fullscreen,
-                    })}
-            >
-                <svg height="13px" viewBox="0 0 14 14" width="14px"
-                    ><title /><desc /><defs /><g
-                        fill-rule="evenodd"
-                        stroke-width="1"
-                        ><g transform="translate(-215.000000, -257.000000)"
-                            ><g transform="translate(215.000000, 257.000000)"
-                                ><path
-                                    d="M2,9 L0,9 L0,14 L5,14 L5,12 L2,12 L2,9 L2,9 Z M0,5 L2,5 L2,2 L5,2 L5,0 L0,0 L0,5 L0,5 Z M12,12 L9,12 L9,14 L14,14 L14,9 L12,9 L12,12 L12,12 Z M9,0 L9,2 L12,2 L12,5 L14,5 L14,0 L9,0 L9,0 Z"
-                                    id="Shape"
-                                /></g
-                            ></g
-                        ></g
-                    ></svg
+            <div class="toolbar">
+                <slot name="extra" />
+                <Button
+                    tip={$config.getLocale().ui.description.collapse}
+                    action={() => dispatch('mode', { mode: Mode.Collapsed })}
+                    active={!layout.isFullscreen()}>⎵</Button
                 >
-            </Button>
+                <Button
+                    tip={$config.getLocale().ui.description.fullscreen}
+                    action={() =>
+                        dispatch('fullscreen', {
+                            fullscreen: !fullscreen,
+                        })}
+                >
+                    <svg height="13px" viewBox="0 0 14 14" width="14px"
+                        ><title /><desc /><defs /><g
+                            fill-rule="evenodd"
+                            stroke-width="1"
+                            ><g transform="translate(-215.000000, -257.000000)"
+                                ><g
+                                    transform="translate(215.000000, 257.000000)"
+                                    ><path
+                                        d="M2,9 L0,9 L0,14 L5,14 L5,12 L2,12 L2,9 L2,9 Z M0,5 L2,5 L2,2 L5,2 L5,0 L0,0 L0,5 L0,5 Z M12,12 L9,12 L9,14 L14,14 L14,9 L12,9 L12,12 L12,12 Z M9,0 L9,2 L12,2 L12,5 L14,5 L14,0 L9,0 L9,0 Z"
+                                        id="Shape"
+                                    /></g
+                                ></g
+                            ></g
+                        ></svg
+                    >
+                </Button>
+            </div>
         </div>
         <!-- Render the content -->
         <div class="content" on:scroll={() => dispatch('scroll')}>
@@ -324,27 +327,34 @@
         transition: none;
     }
 
-    .controls {
+    .header {
         position: relative;
         align-self: start;
         display: flex;
         flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: right;
+        flex-wrap: nowrap;
         align-items: center;
         padding-top: var(--wordplay-spacing);
-        padding-left: calc(2 * var(--wordplay-spacing));
+        padding-left: var(--wordplay-spacing);
         padding-right: var(--wordplay-spacing);
         padding-bottom: 0;
-        min-width: 100%;
-        min-height: 2em;
         gap: var(--wordplay-spacing);
-
-        overflow: scroll;
+        width: 100%;
+        min-height: 2em;
+        overflow-x: auto;
     }
 
-    .controls :global(button) {
+    .header :global(button) {
         opacity: 0.5;
+    }
+
+    .toolbar {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+        min-width: max-content;
+        gap: var(--wordplay-spacing);
     }
 
     .footer {
