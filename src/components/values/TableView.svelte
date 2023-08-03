e8921274b8789fefbd8b9a39e7995ffb7d51f85e
Amy J. Ko <ajko@uw.edu>
2023-08-02 18:04:24 -0700

    Fixed TableView.

--- src/components/values/TableView.svelte
+++ src/components/values/TableView.svelte
@@ -2,19 +2,34 @@
 
 <script lang="ts">
     import type Table from '@runtime/Table';
-    import { TABLE_OPEN_SYMBOL } from '@parser/Symbols';
+    import { TABLE_CLOSE_SYMBOL, TABLE_OPEN_SYMBOL } from '@parser/Symbols';
     import SymbolView from './SymbolView.svelte';
     import Symbol from '@nodes/Symbol';
     import { config } from '../../db/Creator';
+    import ValueView from './ValueView.svelte';
 
     export let value: Table;
 </script>
 
-{#each value.literal.type.columns as col}
+<SymbolView
+    symbol={TABLE_OPEN_SYMBOL}
+    type={Symbol.TableOpen}
+/>{#each value.literal.type.columns as col}
     <SymbolView
         symbol={col
             ? col.names.getPreferredNameString($config.getLocales())
             : ''}
         type={Symbol.Name}
-    /><SymbolView symbol={TABLE_OPEN_SYMBOL} type={Symbol.TableOpen} />
+    />
+{/each}<SymbolView symbol={TABLE_CLOSE_SYMBOL} type={Symbol.TableClose} />
+<br />
+{#each value.rows as row}
+    <SymbolView
+        symbol={TABLE_OPEN_SYMBOL}
+        type={Symbol.TableOpen}
+    />{#each value.literal.type.columns as col}
+        {@const cell = row.resolve(col.names)}
+        {#if cell}<ValueView value={cell} />{:else}-{/if}
+    {/each}<SymbolView symbol={TABLE_CLOSE_SYMBOL} type={Symbol.TableClose} />
+    <br />
 {/each}
