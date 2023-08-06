775f684eaed602be21c41fc039e5c8a57f2bfead
Amy J. Ko <ajko@uw.edu>
2023-08-06 12:32:59 -0700

    Fixed test.

--- src/values/NoneValue.test.ts
+++ src/values/NoneValue.test.ts
@@ -1,11 +1,11 @@
+import { test, expect } from 'vitest';
 import { FALSE_SYMBOL, TRUE_SYMBOL } from '@parser/Symbols';
 import Evaluator from '@runtime/Evaluator';
 import { DefaultLocale } from '../db/Creator';
 
 test.each([
-    ['! = !', TRUE_SYMBOL],
-    ['! = !nan', FALSE_SYMBOL],
-    ['! = 1', FALSE_SYMBOL],
+    ['ø = ø', TRUE_SYMBOL],
+    ['ø ≠ ø', FALSE_SYMBOL],
 ])('Expect %s to be %s', (code, value) => {
     expect(Evaluator.evaluateCode(DefaultLocale, code)?.toString()).toBe(value);
 });
