1909bb1ce319504d0af8575a46f303bfa2357904
Amy J. Ko <ajko@uw.edu>
2023-08-17 09:37:47 -0700

    Don't connect to Firebase if api key is empty.

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -18,27 +18,29 @@ let auth: Auth | undefined;
 let firestore: Firestore | undefined;
 
 try {
-    const firebaseConfig = {
-        apiKey: PUBLIC_FIREBASE_API_KEY,
-        authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: PUBLIC_FIREBASE_PROJECT_ID,
-        messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: PUBLIC_FIREBASE_APP_ID,
-    };
+    if (PUBLIC_FIREBASE_API_KEY.length > 0) {
+        const firebaseConfig = {
+            apiKey: PUBLIC_FIREBASE_API_KEY,
+            authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+            projectId: PUBLIC_FIREBASE_PROJECT_ID,
+            messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: PUBLIC_FIREBASE_APP_ID,
+        };
 
-    // Initialize Firebase
-    const app =
-        getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+        // Initialize Firebase
+        const app =
+            getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-    auth = getAuth(app);
-    firestore = getFirestore(app);
-    // export const functions = getFunctions(app);
+        auth = getAuth(app);
+        firestore = getFirestore(app);
+        // export const functions = getFunctions(app);
 
-    // Initialize emulator if environment is local.
-    if (PUBLIC_CONTEXT === 'local') {
-        connectFirestoreEmulator(firestore, 'localhost', 8080);
-        connectAuthEmulator(auth, 'http://localhost:9099');
-        // connectFunctionsEmulator(functions, 'localhost', 5001);
+        // Initialize emulator if environment is local.
+        if (PUBLIC_CONTEXT === 'local') {
+            connectFirestoreEmulator(firestore, 'localhost', 8080);
+            connectAuthEmulator(auth, 'http://localhost:9099');
+            // connectFunctionsEmulator(functions, 'localhost', 5001);
+        }
     }
 } catch (err) {
     console.log('*** NO ACCESS TO FIREBASE ***');
