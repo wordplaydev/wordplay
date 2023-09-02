bcc448e0ee491a8769b31fdc67187a14f447553b
Amy J. Ko <ajko@uw.edu>
2023-09-02 08:31:53 -0700

    Richer landing page.

--- src/components/app/BigLink.svelte
+++ src/components/app/BigLink.svelte
@@ -2,13 +2,20 @@
     import Link from './Link.svelte';
 
     export let to: string;
+    export let subtitle: string;
 </script>
 
-<Link {to}><span class="big"><slot /></span></Link>
+<p><Link {to}><slot /></Link><span class="subtitle"><br />{subtitle}</span></p>
 
 <style>
-    .big {
+    p {
         font-size: min(6vw, 32pt);
-        margin-bottom: var(--wordplay-spacing);
+        margin-block-end: 1em;
+        line-height: 0.85;
+    }
+
+    .subtitle {
+        color: var(--wordplay-header);
+        font-size: min(4vw, 16pt);
     }
 </style>
