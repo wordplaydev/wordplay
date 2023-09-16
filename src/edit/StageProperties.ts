ba6eee3d0a00456fa062a427c3c31d10f86b2ba8
Amy J. Ko <ajko@uw.edu>
2023-09-16 09:03:58 -0700

    Editable gravity.

--- src/edit/StageProperties.ts
+++ src/edit/StageProperties.ts
@@ -1,7 +1,10 @@
 import type Locale from '../locale/Locale';
 import type Project from '../models/Project';
 import ListLiteral from '../nodes/ListLiteral';
+import NumberLiteral from '../nodes/NumberLiteral';
+import Unit from '../nodes/Unit';
 import OutputProperty from './OutputProperty';
+import OutputPropertyRange from './OutputPropertyRange';
 
 export default function getStageProperties(
     project: Project,
@@ -15,6 +18,14 @@ export default function getStageProperties(
             false,
             (expr) => expr instanceof ListLiteral,
             () => ListLiteral.make([])
+        ),
+        new OutputProperty(
+            locale.output.Stage.gravity,
+            new OutputPropertyRange(0, 20, 1, 'm/s^2'),
+            true,
+            false,
+            (expr) => expr instanceof NumberLiteral,
+            () => NumberLiteral.make('9.8', Unit.create(['m'], ['s', 's']))
         ),
     ];
 }
