f67219d70071448485a7d2fdcbdd31398c748efc
Amy J. Ko <ajko@uw.edu>
2023-10-07 10:58:09 -0700

    Removed emulator dependency on production credentials.

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -28,33 +28,29 @@ let analytics: Analytics | undefined = undefined;
 // Don't connect to firebase when running in node.
 if (typeof process === 'undefined') {
     try {
-        if (PUBLIC_FIREBASE_API_KEY.length > 0) {
-            const firebaseConfig = {
-                apiKey: PUBLIC_FIREBASE_API_KEY,
-                authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-                projectId: PUBLIC_FIREBASE_PROJECT_ID,
-                messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-                appId: PUBLIC_FIREBASE_APP_ID,
-                measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID,
-            };
+        const firebaseConfig = {
+            apiKey: PUBLIC_FIREBASE_API_KEY,
+            authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+            projectId: PUBLIC_FIREBASE_PROJECT_ID,
+            messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: PUBLIC_FIREBASE_APP_ID,
+            measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID,
+        };
 
-            // Initialize Firebase
-            const app =
-                getApps().length === 0
-                    ? initializeApp(firebaseConfig)
-                    : getApp();
+        // Initialize Firebase
+        const app =
+            getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-            auth = getAuth(app);
-            firestore = getFirestore(app);
-            functions = getFunctions(app);
-            analytics = getAnalytics(app);
+        auth = getAuth(app);
+        firestore = getFirestore(app);
+        functions = getFunctions(app);
+        analytics = getAnalytics(app);
 
-            // Initialize emulator if environment is local.
-            if (PUBLIC_CONTEXT === 'local') {
-                connectFirestoreEmulator(firestore, 'localhost', 8080);
-                connectAuthEmulator(auth, 'http://localhost:9099');
-                connectFunctionsEmulator(functions, 'localhost', 5001);
-            }
+        // Initialize emulator if environment is local.
+        if (PUBLIC_CONTEXT === 'local') {
+            connectFirestoreEmulator(firestore, 'localhost', 8080);
+            connectAuthEmulator(auth, 'http://localhost:9099');
+            connectFunctionsEmulator(functions, 'localhost', 5001);
         }
     } catch (err) {
         console.log('*** NO ACCESS TO FIREBASE ***');
