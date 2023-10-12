6e0bf4b2990685414fb85105f456f41d79d9b491
Amy J. Ko <ajko@uw.edu>
2023-10-11 20:07:35 -0700

    Moved vite aliases to svelete config due to breaking change in vite update.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +14,22 @@ const config = {
             assets: 'build',
             fallback: 'index.html',
         }),
+        alias: {
+            '@components': path.resolve('./src/components'),
+            '@nodes': path.resolve('./src/nodes'),
+            '@runtime': path.resolve('./src/runtime'),
+            '@values': path.resolve('./src/values'),
+            '@conflicts': path.resolve('./src/conflicts'),
+            '@locale': path.resolve('./src/locale'),
+            '@concepts': path.resolve('./src/concepts'),
+            '@parser': path.resolve('./src/parser'),
+            '@input': path.resolve('./src/input'),
+            '@output': path.resolve('./src/output'),
+            '@basis': path.resolve('./src/basis'),
+            '@edit': path.resolve('./src/edit'),
+            '@models': path.resolve('./src/models'),
+            '@db': path.resolve('./src/db'),
+        },
     },
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import eslint from 'vite-plugin-eslint';
-import path from 'path';
 
 /**
  * Make a little plugin that checks for locale file changes and fires an event.
@@ -24,24 +23,6 @@ function LocaleHotReload() {
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit(), LocaleHotReload(), eslint()],
-    resolve: {
-        alias: {
-            '@components': path.resolve('./src/components'),
-            '@nodes': path.resolve('./src/nodes'),
-            '@runtime': path.resolve('./src/runtime'),
-            '@values': path.resolve('./src/values'),
-            '@conflicts': path.resolve('./src/conflicts'),
-            '@locale': path.resolve('./src/locale'),
-            '@concepts': path.resolve('./src/concepts'),
-            '@parser': path.resolve('./src/parser'),
-            '@input': path.resolve('./src/input'),
-            '@output': path.resolve('./src/output'),
-            '@basis': path.resolve('./src/basis'),
-            '@edit': path.resolve('./src/edit'),
-            '@models': path.resolve('./src/models'),
-            '@db': path.resolve('./src/db'),
-        },
-    },
 };
 
 export default config;
