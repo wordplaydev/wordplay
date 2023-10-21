8c812279c98b07a9f1eef8f684848a3257cd3c71
Amy J. Ko <ajko@uw.edu>
2023-10-21 14:30:10 -0700

    Resolved timeline command conflicts

--- src/components/widgets/CommandHint.svelte
+++ src/components/widgets/CommandHint.svelte
@@ -7,7 +7,8 @@
     const modifiers = getKeyboardModifiers();
 </script>
 
-{#if $modifiers && ((command.control && $modifiers.control) || (command.shift && $modifiers.shift) || (command.alt && $modifiers.alt))}<div
+<!-- Show the hint if any modifier is pressed and the pattern of modifier keys pressed partially matches this command -->
+{#if $modifiers && Object.values($modifiers).some((val) => val === true) && ((command.control && $modifiers.control) || (command.shift && command.shift) || (command.alt && $modifiers.alt)) && !((command.control === false && $modifiers.control) || (command.shift && $modifiers.shift) || (command.alt && $modifiers.alt))}<div
         class="hint"
         role="presentation"
         >{toShortcut(
