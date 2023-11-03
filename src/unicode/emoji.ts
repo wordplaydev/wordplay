54099fc709d8cc38e7f34e688ecfc063adce3d67
Amy J. Ko <ajko@uw.edu>
2023-11-02 18:54:52 -0700

    Fixed emoji detection to exclude numbers.

--- src/unicode/emoji.ts
+++ src/unicode/emoji.ts
@@ -1,4 +1,7 @@
 /** Adds emoji color variation descriptor to any missing them. Ensures fonts are selected correctly in Safari. */
 export function withVariationSelector(text: string) {
-    return text.replaceAll(/(\p{Emoji}(?!\uFE0F))/gu, '$1\uFE0F');
+    return text.replaceAll(
+        /(\p{Extended_Pictographic}(?!\uFE0F))/gu,
+        '$1\uFE0F'
+    );
 }
