1c106d1a838d8802231b65a1b9420d4b09caf685
Amy J. Ko <ajko@uw.edu>
2023-11-20 11:24:22 -0800

    Distinguished fullscreen of tile from fullscreen of project.

--- src/components/project/TileView.svelte
+++ src/components/project/TileView.svelte
@@ -25,10 +25,10 @@
     import Glyphs from '../../lore/Glyphs';
     import Color from '../../output/Color';
     import Toggle from '../widgets/Toggle.svelte';
-    import { EnterFullscreen, ExitFullscreen } from '../editor/util/Commands';
     import type Project from '../../models/Project';
     import Emoji from '@components/app/Emoji.svelte';
     import TileSymbols from './TileSymbols';
+    import FullscreenIcon from './FullscreenIcon.svelte';
 
     export let project: Project;
     export let tile: Tile;
@@ -231,31 +231,12 @@
                 <Toggle
                     tips={$locales.get((l) => l.ui.tile.toggle.fullscreen)}
                     on={fullscreen}
-                    command={fullscreen ? ExitFullscreen : EnterFullscreen}
                     toggle={() =>
                         dispatch('fullscreen', {
                             fullscreen: !fullscreen,
                         })}
                 >
-                    <svg
-                        height="13px"
-                        viewBox="0 0 14 14"
-                        width="14px"
-                        style="stroke: currentColor; fill: currentColor; pointer-events: none;"
-                        ><title /><desc /><defs /><g
-                            fill-rule="evenodd"
-                            stroke-width="1"
-                            ><g transform="translate(-215.000000, -257.000000)"
-                                ><g
-                                    transform="translate(215.000000, 257.000000)"
-                                    ><path
-                                        d="M2,9 L0,9 L0,14 L5,14 L5,12 L2,12 L2,9 L2,9 Z M0,5 L2,5 L2,2 L5,2 L5,0 L0,0 L0,5 L0,5 Z M12,12 L9,12 L9,14 L14,14 L14,9 L12,9 L12,12 L12,12 Z M9,0 L9,2 L12,2 L12,5 L14,5 L14,0 L9,0 L9,0 Z"
-                                        id="Shape"
-                                    /></g
-                                ></g
-                            ></g
-                        ></svg
-                    >
+                    <FullscreenIcon />
                 </Toggle>
                 {#if !layout.isFullscreen()}
                     <Button
