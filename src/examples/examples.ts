a6f067d049524d9e0cfed0693b559a6eab1c4241
Vincent <vincentkao2003@gmail.com>
2023-11-29 14:24:09 -0800

    Basketball Star Gallery Example

--- src/examples/examples.ts
+++ src/examples/examples.ts
@@ -105,7 +105,7 @@ export function getExampleGalleries(locales: Locales): Gallery[] {
             Object.fromEntries(
                 locale.map((l) => [toLocaleString(l), l.gallery.games])
             ),
-            ['Adventure', 'Maze', 'WhatWord', 'Catch', 'Madlib', 'WheresWaldough']
+            ['Adventure', 'BasketballStar', 'Maze', 'WhatWord', 'Catch', 'Madlib', 'WheresWaldough']
         ),
         createGallery(
             'Visualizations',
