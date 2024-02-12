86b74079089389e9498cf36458822fdfaa66c70b
lpjjj1222 <694676764@qq.com>
2024-02-11 22:21:23 -0800

    Revert changes to package.json and package-lock.json. Also solve re-render bug.

--- src/components/editor/Space.svelte
+++ src/components/editor/Space.svelte
@@ -5,13 +5,13 @@
     import InsertionPointView from './InsertionPointView.svelte';
     import type { InsertionPoint } from '../../edit/Drag';
     import { EXPLICIT_TAB_TEXT, TAB_TEXT } from '../../parser/Spaces';
-    import {locales, DB, spaceIndicator} from '../../db/Database';
+    import { locales, DB, spaceIndicator } from '../../db/Database';
 
     export let token: Token;
     export let space: string;
     export let additional: string;
     export let insertion: InsertionPoint | undefined = undefined;
-    
+
     $: insertionIndex =
         insertion !== undefined
             ? space.split('\n', insertion.line).join('\n').length + 1
@@ -25,16 +25,26 @@
     $: afterSpaces = render(
         insertionIndex === undefined ? space : space.substring(insertionIndex),
         true,
-        $spaceIndicator
+        $spaceIndicator,
     );
 
     $: additionalSpaces =
-        additional.length === 0 ? [] : render(additional, false, $spaceIndicator);
+        additional.length === 0
+            ? []
+            : render(additional, false, $spaceIndicator);
 
-    function render(text: string, explicit: boolean, Indicator:boolean): string[] {
+    function render(
+        text: string,
+        explicit: boolean,
+        Indicator: boolean,
+    ): string[] {
         return (
             explicit
-                ? (Indicator ? text.replaceAll(' ', '·').replaceAll('\t', EXPLICIT_TAB_TEXT):text.replaceAll(' ', '\xa0').replaceAll('\t', TAB_TEXT))
+                ? Indicator
+                    ? text
+                          .replaceAll(' ', '·')
+                          .replaceAll('\t', EXPLICIT_TAB_TEXT)
+                    : text.replaceAll(' ', '\xa0').replaceAll('\t', TAB_TEXT)
                 : text.replaceAll(' ', '\xa0').replaceAll('\t', TAB_TEXT)
         ).split('\n');
     }
@@ -44,21 +54,23 @@
     This monstrosity renders space, accounting for insertion points. We key on space
     to work around a Svelte defect that doesn't correctly update changes in text nodes.
 -->
-{#key space}
-    <span class="space" role="none" data-id={token.id}
-        ><span role="none" class="before"
-            >{#each beforeSpaces as s, index}{#if index > 0}<span
-                        ><br class="break" /></span
-                    >{/if}{#if s === ''}&ZeroWidthSpace;{:else}{s}{/if}{:else}&ZeroWidthSpace;{/each}{#if insertion}<InsertionPointView
-                />{/if}</span
-        ><span role="none" class="after"
-            >{#each afterSpaces as s, index}{#if index > 0}<span
-                        ><br class="break" /></span
-                    >{/if}{s}{/each}{#each additionalSpaces as s, index}{#if index > 0}<span
-                        ><br class="break" /></span
-                    >{/if}{#if s === ''}&ZeroWidthSpace;{:else}{s}{/if}{/each}</span
-        ></span
-    >
+{#key $spaceIndicator}
+    {#key space}
+        <span class="space" role="none" data-id={token.id}
+            ><span role="none" class="before"
+                >{#each beforeSpaces as s, index}{#if index > 0}<span
+                            ><br class="break" /></span
+                        >{/if}{#if s === ''}&ZeroWidthSpace;{:else}{s}{/if}{:else}&ZeroWidthSpace;{/each}{#if insertion}<InsertionPointView
+                    />{/if}</span
+            ><span role="none" class="after"
+                >{#each afterSpaces as s, index}{#if index > 0}<span
+                            ><br class="break" /></span
+                        >{/if}{s}{/each}{#each additionalSpaces as s, index}{#if index > 0}<span
+                            ><br class="break" /></span
+                        >{/if}{#if s === ''}&ZeroWidthSpace;{:else}{s}{/if}{/each}</span
+            ></span
+        >
+    {/key}
 {/key}
 
 <style>
