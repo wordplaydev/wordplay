ef632a8749b4606dcd468d2a75c0f5145725d3b9
Deleted-0970 <derekliuj@gmail.com>
2024-02-13 21:01:35 -0800

    return resolution type

--- src/conflicts/OrderOfOperations.ts
+++ src/conflicts/OrderOfOperations.ts
@@ -1,25 +1,19 @@
-// PSEUDO CODE!
-// Check what datatypes were passed in
-// iterate to see where all the operators are
-// put parenthesis around the objects that are next to the operators
-// return as a locales object?
-//
-
 // We need to return a resolution type. The resolution type creates an instance of the project and tests ur code
 // We need to write our code within getConflictNodes(), not create our own method
 
-// PossiblePII.ts is the only working conflict resolution so far.
+// PossiblePII.ts is the only working conflict resolution so far. 
 // To test it out:
 // 1. npm run dev
 // 2. create a new project
 // 3. write a string containing PII such as an email (e.g. 'bob@gmail.com')
 // 4. a warning should be displayed. If the button is clicked, the PII will be added to something...
 
-// This week, we need to come up with a design draft
-// Current thoughts:
-//	- create a similar message as the on in PII that tells the user about PEMDAS and offer an option to evaluate equations
-//	  in-order or with PEMDAS. This means we only need to write something that will evaluate equations in terms of PEMDAS
-//	  or in-order depending on what the user chooses.
+// Current progress:
+// - returning a resolution type and created a placeholder mediator
+// - created text for the client on what will be displayed
+// Help needed to proceed:
+// - how to integrate the word translation part (locales?) into the text displayed to the user
+// - how to get started on working the mediator and making the button actually do something
 
 import type BinaryEvaluate from '@nodes/BinaryEvaluate';
 import Conflict from './Conflict';
@@ -37,8 +31,8 @@ export default class OrderOfOperations extends Conflict {
         this.after = after;
     }
 
-    // // splits String expression. Uses a recursive method to find all combinations of equations
-    // // by adding parenthesis in different places in the equations.
+    // // splits String expression. Uses a recursive method to find all combinations of equations 
+    // // by adding parenthesis in different places in the equations. 
     // generateDisambiguations(expression: string): string[] {
     //     const parts = expression.split(' ');
     //     let disambiguations: string[] = [];
@@ -60,22 +54,55 @@ export default class OrderOfOperations extends Conflict {
     //     addParentheses(0, parts.length - 1);
     //     return disambiguations;
     // }
+    
 
-    getConflictingNodes() {
+	// SAVE ORIGINAL STATE OF getConflictNodes IN CASE OF FAILURE
+    // getConflictingNodes() {
+    //     return {
+    //         primary: {
+    //             node: this.operation,
+				// // this translates text into the specificed language
+    //             explanation: (locales: Locales) =>
+    //                 concretize(
+    //                     locales,
+    //                     locales.get(
+    //                         (l) =>
+    //                             l.node.BinaryEvaluate.conflict.OrderOfOperations
+    //                     )
+    //                 ),
+    //         },
+    //     };
+    // }
+	getConflictingNodes() {
         return {
             primary: {
                 node: this.operation,
-                // this translates text into the specificed language
+				// this translates text into the specificed language
                 explanation: (locales: Locales) =>
                     concretize(
                         locales,
                         locales.get(
                             (l) =>
-                                l.node.BinaryEvaluate.conflict
-                                    .OrderOfOperations,
-                        ),
+							'I evaluate in reading order. Traditionally, mathematical expressions are evaluated in the order of Parenthesis, Exponents, Multiplication and Division, and finally Addition and Subtraction (PEMDAS). Would you like me to evaluate using PEMDAS?'
+						)
                     ),
             },
+			resolutions: [
+				{
+					description: (locales: Locales) =>
+                        concretize(
+                            locales,
+                            locales.get(
+                                (l) => `use PEMDAS`
+						),
+					),
+					mediator: (context: Context) => {
+                        // enforce PEMDAS here...
+						// no idea how to do this ask Amy on wednesday
+                        // return context.project.enforcePEMDAS(this.operation);
+                    },
+				}
+			]
         };
     }
 }
