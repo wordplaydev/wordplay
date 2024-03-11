6e474ab18dc7b6ae242050bee34680cacb008222
George <bob888w@uw.edu>
2024-03-10 18:21:55 -0700

    limit test to homepage

--- tests/WCAG2.AxeBuilder.Subtitle.test.ts
+++ tests/WCAG2.AxeBuilder.Subtitle.test.ts
@@ -1,7 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright'; 
 
-//Print out the accessibility scan results from AxeBuilder.
+// Print out the accessibility scan results from AxeBuilder.
 function printAccessibilityScanResults(axeBuilderScanResults: any){
     let violations = axeBuilderScanResults.violations;
     violations.forEach((violation) => {
@@ -41,67 +41,3 @@ test('Test Wordplay homepage subtitles for WCAG violations',
   }
   printAccessibilityScanResults(accessibilityScanResults);
 });
-
-async function clickLinkAndTestSubtitlesWCAG(page:Page,linkToClick: string) {  
-    await page.goto('/');
-
-    // Click the first link.
-    await page.getByRole('link', { name: linkToClick }).first().click();
-    
-    // Ensure page is loaded for testing.
-    await page.waitForURL('**/' + linkToClick.toLowerCase());
-    
-    // Use AxeBuilder to scan subtitles for acessiblity violations
-    // with CSS selector of ".link>.subtitle".
-    let accessibilityScanResults : any;
-    try {
-      accessibilityScanResults = await new AxeBuilder({ page })
-      // Use CSS selector to include only subtitle class for WCAG test,
-      // e.g., the "Why does this place exist?" <span> element.
-      .include('.link>.subtitle')
-      // Limit analysis to WCAGs.
-      .withTags(['wcag2a', 'wcag2aa','wcag2aaa'])
-      .analyze();
-    } catch (cannotFindSubtitleLink) {
-      console.log("No subtitle(s) found\n");
-      return;
-    }
-    printAccessibilityScanResults(accessibilityScanResults);
-}
-
-// Do subtitle tests for all main pages.
-test('Test Wordplay "Learn" page subtitles for WCAG violations', 
-  async ({ page }) => {
-    await clickLinkAndTestSubtitlesWCAG(page, 'Learn');
-});
-    
-test('Test Wordplay "Projects" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'Projects');
-});
- 
-test('Test Wordplay "Galleries" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'Galleries');
-});
-
-test('Test Wordplay "Login" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'Login');
-});
- 
-test('Test Wordplay "About" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'About');
-});
- 
-test('Test Wordplay "Rights" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'Rights');
-});
-
-test('Test Wordplay "Donate" page subtitles for WCAG violations',
-    async ({ page }) => {
-      await clickLinkAndTestSubtitlesWCAG(page, 'Donate');
-});
- 
