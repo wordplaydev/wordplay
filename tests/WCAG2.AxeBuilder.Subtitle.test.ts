c243be5f25e0d4e9908d62c654cb07d2069ac2e1
George <bob888w@uw.edu>
2024-03-10 18:55:10 -0700

    removed redudant page type

--- tests/WCAG2.AxeBuilder.Subtitle.test.ts
+++ tests/WCAG2.AxeBuilder.Subtitle.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright'; 
 
 // Print out the accessibility scan results from AxeBuilder.
