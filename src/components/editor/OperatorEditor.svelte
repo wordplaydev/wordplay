1b5f49ae9287e8efe4e184d5b7c8599aa1b0120c
Amy J. Ko <ajko@uw.edu>
2024-07-20 16:07:02 -0700

    Custom drop down menu for blocks editor.

--- src/components/editor/OperatorEditor.svelte
+++ src/components/editor/OperatorEditor.svelte
@@ -1,33 +1,27 @@
 <script lang="ts">
-    import Options from '@components/widgets/Options.svelte';
-    import { Projects } from '@db/Database';
+    import { locales, Projects } from '@db/Database';
     import type Project from '@models/Project';
     import type BinaryEvaluate from '@nodes/BinaryEvaluate';
     import Context from '@nodes/Context';
     import type UnaryEvaluate from '@nodes/UnaryEvaluate';
     import Reference from '@nodes/Reference';
+    import CodeOptions from './CodeOptions.svelte';
 
     export let project: Project;
-    export let binary: BinaryEvaluate | UnaryEvaluate;
+    export let evaluate: BinaryEvaluate | UnaryEvaluate;
     export let context: Context;
-    export let operator: string;
     export let placeholder: string;
 </script>
 
-<Options
-    value={operator}
+<CodeOptions
+    current={evaluate.fun.name}
     label={placeholder ?? ''}
     change={(op) =>
-        op
-            ? Projects.revise(project, [[binary.fun, Reference.make(op)]])
-            : undefined}
-    width="fit-content"
-    code
-    options={binary.getFunctions(context).map((def) => {
-        const value = def.getNames()[0];
-        return {
-            value,
-            label: value,
-        };
-    })}
+        op ? Projects.revise(project, [[evaluate.fun, op]]) : undefined}
+    options={() =>
+        evaluate
+            .getFunctions(context)
+            .map((def) =>
+                Reference.make(def.getPreferredName($locales.getLocales())),
+            )}
 />
--- src/components/editor/ReferenceEditor.svelte
+++ src/components/editor/ReferenceEditor.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import Options from '@components/widgets/Options.svelte';
     import { Projects } from '@db/Database';
     import type Project from '@models/Project';
     import Context from '@nodes/Context';
     import Reference from '@nodes/Reference';
+    import CodeOptions from './CodeOptions.svelte';
 
     export let project: Project;
     export let reference: Reference;
@@ -11,20 +11,13 @@
     export let placeholder: string;
 </script>
 
-<Options
-    value={reference.getName()}
+<CodeOptions
+    current={reference.name}
     label={placeholder ?? ''}
     change={(op) =>
-        op
-            ? Projects.revise(project, [[reference, Reference.make(op)]])
-            : undefined}
-    width="fit-content"
-    code
-    options={reference.getDefinitionsInScope(context).map((def) => {
-        const value = def.getNames()[0];
-        return {
-            value,
-            label: value,
-        };
-    })}
+        op ? Projects.revise(project, [[reference, op]]) : undefined}
+    options={() =>
+        reference
+            .getDefinitionsInScope(context)
+            .map((def) => Reference.make(def.getNames()[0], def))}
 />
