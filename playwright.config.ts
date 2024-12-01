291356d2bab6d21aa38db734d6b4a9a5738fc420
Amy J. Ko <ajko@uw.edu>
2024-11-30 16:49:03 -0800

    Adjust test configuration GitHub Actions to minimize crashes.

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,14 +11,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
     testDir: './tests/end2end',
-    /* Run tests in files in parallel */
-    fullyParallel: true,
+    /* Run tests in files in parallel unless on CI */
+    fullyParallel: !process.env.CI,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
