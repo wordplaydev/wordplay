b9859d6d22e70da92379506c18e46013f0d6e1b1
Amy J. Ko <ajko@uw.edu>
2024-12-23 10:15:47 -0800

    Defined the schemas for classes and learners.

---
+++ src/db/TeacherDatabase.svelte.ts
@@ -0,0 +1,27 @@
+// Provide schemas and database access for all teaching functionality.
+// By design, we get all data on demand here, rather than caching, using a
+// more transactional model.
+
+/** Represents a learner record that the teacher has created. */
+export type Learner = {
+    /** The Firebase UID of the learner. */
+    id: number;
+    /** The username of the learner. */
+    username: string;
+    /** The list of metadata provided by the teacher about the learner, to help them recognize the learner. */
+    metadata: string[];
+};
+
+/** Represents a class that that the teacher has created. */
+export type Class = {
+    /** A short description of the class, to help the teacher recognize and distinguish it from other classes. */
+    name: string;
+    /** A long description of the class, to help a teacher provide instructions and context to students. */
+    description: string;
+    /** A set of user IDs representing the teachers who can manage the learners in the class. */
+    teachers: string[]; // The teacher uids that oversee the class
+    /** The set of learners in the class */
+    learners: Learner[];
+    /** The galleries associated with the class, so that we can show the association on a gallery page */
+    galleries: string[]; // The set of galleries associated with the group
+};
