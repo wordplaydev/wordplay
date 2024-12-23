a4a021f50f9e3fd13f80c0910ce0f3e7e8af7fe0
Amy J. Ko <ajko@uw.edu>
2024-12-23 10:09:05 -0800

    Factored out a teachers only component to reuse claim check.

---
+++ src/routes/TeachersOnly.svelte
@@ -0,0 +1,39 @@
+<!-- A reusable component that only shows its content if the user is a teacher -->
+<script lang="ts">
+    import Link from '@components/app/Link.svelte';
+    import Spinning from '@components/app/Spinning.svelte';
+    import MarkupHtmlView from '@components/concepts/MarkupHTMLView.svelte';
+    import { getUser } from '@components/project/Contexts';
+    import { locales } from '@db/Database';
+    import getClaim from '@models/getClaim';
+    import type { Snippet } from 'svelte';
+
+    let { children }: { children: Snippet } = $props();
+
+    let user = getUser();
+</script>
+
+{#if $user === null}
+    <MarkupHtmlView markup={$locales.get((l) => l.ui.page.teach.error.login)} />
+{:else}
+    {#await getClaim($user, 'teacher')}
+        <Spinning />
+    {:then claim}
+        {#if !claim}
+            <MarkupHtmlView
+                markup={$locales.get((l) => l.ui.page.teach.error.teacher)}
+            />
+            <p>
+                <Link to="https://forms.gle/6x1sbyC4SZHoPXYq5"
+                    >{$locales.get((l) => l.ui.page.teach.link.request)}</Link
+                >
+            </p>
+        {:else}
+            {@render children()}
+        {/if}
+    {:catch}
+        <MarkupHtmlView
+            markup={$locales.get((l) => l.ui.page.teach.error.offline)}
+        />
+    {/await}
+{/if}
--- src/routes/teach/+page.svelte
+++ src/routes/teach/+page.svelte
@@ -3,10 +3,8 @@
     import Writing from '@components/app/Writing.svelte';
     import { locales } from '@db/Database';
     import MarkupHtmlView from '../../components/concepts/MarkupHTMLView.svelte';
-    import getClaim from '@models/getClaim';
-    import Spinning from '@components/app/Spinning.svelte';
     import { getUser } from '@components/project/Contexts';
-    import Link from '@components/app/Link.svelte';
+    import TeachersOnly from '../TeachersOnly.svelte';
 
     let user = getUser();
 </script>
@@ -17,34 +15,9 @@
 
 <Writing>
     <Header>{$locales.get((l) => l.ui.page.teach.header)}</Header>
-    {#if $user === null}
+    <TeachersOnly>
         <MarkupHtmlView
-            markup={$locales.get((l) => l.ui.page.teach.error.login)}
+            markup={$locales.get((l) => l.ui.page.teach.prompt.some)}
         />
-    {:else}
-        {#await getClaim($user, 'teacher')}
-            <Spinning />
-        {:then claim}
-            {#if !claim}
-                <MarkupHtmlView
-                    markup={$locales.get((l) => l.ui.page.teach.error.teacher)}
-                />
-                <p>
-                    <Link to="https://forms.gle/6x1sbyC4SZHoPXYq5"
-                        >{$locales.get(
-                            (l) => l.ui.page.teach.link.request,
-                        )}</Link
-                    >
-                </p>
-            {:else}
-                <MarkupHtmlView
-                    markup={$locales.get((l) => l.ui.page.teach.prompt.some)}
-                />
-            {/if}
-        {:catch}
-            <MarkupHtmlView
-                markup={$locales.get((l) => l.ui.page.teach.error.offline)}
-            />
-        {/await}
-    {/if}
+    </TeachersOnly>
 </Writing>
