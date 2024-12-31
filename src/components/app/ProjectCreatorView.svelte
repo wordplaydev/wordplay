461b5c425623a196a1a2b6c43cb4e2503f89cb58
AmyWang <awang040403@gmail.com>
2024-12-30 21:54:39 -0800

    Issue 606: Create ProjectCreatorView.svelte

---
+++ src/components/app/ProjectCreatorView.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+    import type { Creator } from '@db/CreatorDatabase';
+    import Feedback from './Feedback.svelte';
+    import { locales } from '@db/Database';
+
+    interface Props {
+        creator: Creator | null;
+        anonymize?: boolean;
+        chrome?: boolean;
+        fade?: boolean;
+        prompt?: boolean;
+    }
+
+    let {
+        creator,
+        anonymize = true,
+        chrome = true,
+        fade = false,
+        prompt = false,
+    }: Props = $props();
+    let username = $derived(creator?.getProjectUsername(anonymize) ?? '');
+</script>
+
+<div class="creator" class:chrome>
+    {#if creator}<span
+            class="name"
+            style:animation-delay={`${Math.random() * 1000}ms`}
+            >{creator.getName() ?? ''}</span
+        >{/if}{#if creator}
+        {username.length < 1
+            ? username
+            : `${username}`}{:else if prompt}{$locales.get(
+            (l) => l.ui.page.login.anonymous,
+        )}{:else}
+        <Feedback inline>—</Feedback>{/if}</div
+>
+
+<style>
+    .creator {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        gap: var(--wordplay-spacing);
+        align-items: center;
+        justify-content: left;
+        font-size: var(--wordplay-small-font-size);
+    }
+
+    .fade {
+        color: var(--wordplay-inactive-color);
+    }
+
+    .chrome {
+        border-radius: var(--wordplay-border-radius);
+        border-top-left-radius: 1em;
+        border-bottom-left-radius: 1em;
+        border: var(--wordplay-border-color) solid var(--wordplay-border-width);
+        padding: calc(var(--wordplay-spacing) / 3);
+        padding-left: var(--wordplay-spacing);
+    }
+
+    @keyframes rotate {
+        0% {
+            transform: rotate(10deg);
+        }
+        20% {
+            transform: rotate(70deg);
+        }
+        50% {
+            transform: rotate(-50deg);
+        }
+        80% {
+            transform: rotate(10deg);
+        }
+        100% {
+            transform: rotate(0deg);
+        }
+    }
+
+    .name {
+        display: inline-block;
+        font-family: 'Noto Color Emoji', 'Noto Sans';
+        animation: rotate infinite ease-in 5s;
+    }
+</style>
