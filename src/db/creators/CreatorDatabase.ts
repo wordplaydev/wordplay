c414d04d8003257e614a3c0a959f5a1b70391280
AmyWang <awang040403@gmail.com>
2025-01-02 17:25:28 -0800

    Update CreatorDatabase.ts to restore comments

--- src/db/creators/CreatorDatabase.ts
+++ src/db/creators/CreatorDatabase.ts
@@ -1,18 +1,20 @@
 import { httpsCallable } from 'firebase/functions';
-import type { Database } from './Database';
-import { functions } from './firebase';
+import type { Database } from '../Database';
+import { functions } from '../firebase';
 import type { UserIdentifier } from 'firebase-admin/auth';
 import type { User } from 'firebase/auth';
 import isValidEmail from './isValidEmail';
 
 export const CreatorCollection = 'creators';
 
+/** The type for the record returned by our cloud functions */
 type CreatorSchema = {
     uid: string;
     name: string | null;
     email: string | null;
 };
 
+/** Tracks metadata about creators, which is primarily stored in Firebase Auth, but also Firestore, where non-auth data about users lives. */
 export class Creator {
     static CreatorUsernameEmailDomain = '@u.wordplay.dev';
     readonly data: CreatorSchema;
@@ -34,6 +36,12 @@ export class Creator {
 
     static isUsername(email: string) {
         return email.endsWith(Creator.CreatorUsernameEmailDomain);
+    }
+
+    static getUsername(email: string) {
+        return Creator.isUsername(email)
+            ? email.replace(Creator.CreatorUsernameEmailDomain, '')
+            : email;
     }
 
     getName() {
@@ -82,7 +90,10 @@ export class Creator {
 }
 
 export default class CreatorDatabase {
+    /** The main database that manages this gallery database */
     readonly database: Database;
+
+    /** A cache of creator data from Firebase auth */
     private creatorsByEmail = new Map<string, Creator>();
     private creatorsByUID = new Map<string, Creator>();
 
@@ -112,9 +123,13 @@ export default class CreatorDatabase {
             else missing.push(id);
         }
 
+        // Found them all? Return the list.
         if (missing.length === 0) return creators;
+
+        // No access to database? Return what we've got.
         if (functions === undefined) return creators;
 
+        // Get missing info.
         const getCreators = httpsCallable<UserIdentifier[], CreatorSchema[]>(
             functions,
             'getCreators',
@@ -126,6 +141,7 @@ export default class CreatorDatabase {
             )
         ).data as CreatorSchema[];
 
+        // Add the missing creators
         for (const schema of missingCreators) {
             const creator = new Creator(schema);
             if (schema.email) this.creatorsByEmail.set(schema.email, creator);
@@ -136,25 +152,35 @@ export default class CreatorDatabase {
             creators.push(creator);
         }
 
+        // Remember the emails we didn't find users for.
         for (const id of missing)
             (email ? this.unknownEmails : this.unknownUIDs).add(id);
+
+        // Return the final list of creators.
         return creators;
     }
 
     async getCreatorsByUIDs(
         uids: string[],
     ): Promise<Record<string, Creator | null>> {
+        // First get any missing creators.
         await this.getCreators(uids, 'uid');
+
+        // Then construct a mapping
         const map: Record<string, Creator | null> = {};
         for (const uid of uids) map[uid] = this.creatorsByUID.get(uid) ?? null;
         return map;
     }
 
     async getUID(emailOrUsername: string): Promise<string | null> {
+        // Append the username domain if it's not an email
         if (!isValidEmail(emailOrUsername))
             emailOrUsername =
                 emailOrUsername + Creator.CreatorUsernameEmailDomain;
+        // First get any missing creators.
         await this.getCreators([emailOrUsername], 'email');
+
+        // Then return what we've got.
         return this.creatorsByEmail.get(emailOrUsername)?.getUID() ?? null;
     }
 
