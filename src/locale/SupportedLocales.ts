0c3e303284cc1758ee808a71476729ae4f75f457
Amy J. Ko <ajko@uw.edu>
2025-01-11 17:33:41 -0800

    Machine translation for #576.

--- src/locale/SupportedLocales.ts
+++ src/locale/SupportedLocales.ts
@@ -13,6 +13,7 @@ export const EventuallySupportedLocales = [
     'mr-IN',
     'pl-PL',
     'gu-IN',
+    'tr-TR',
 ]; /** A list of locales officially supported by Wordplay. */
 
 /** Supported locale names. Update this list when a locale is ready to share with the world. */
