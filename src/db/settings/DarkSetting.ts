9689eb2d7fb71a785317bd88a0b2aa8ace0bcb05
Amy J. Ko <ajko@uw.edu>
2025-02-08 08:52:57 -0800

    Fixed dark mode loading.

--- src/db/settings/DarkSetting.ts
+++ src/db/settings/DarkSetting.ts
@@ -4,6 +4,6 @@ export const DarkSetting = new Setting<boolean | null>(
     'dark',
     true,
     null,
-    (value) => typeof value === 'boolean' || value === null,
+    (value) => (typeof value === 'boolean' ? value : undefined),
     (current, value) => current === value,
 );
