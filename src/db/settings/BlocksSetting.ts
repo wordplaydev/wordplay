8ab1030336a93610c81ec9d13a47720fdb117f0c
Amy J. Ko <ajko@uw.edu>
2025-08-02 17:39:45 -0700

    Enabled blocks toggle.

--- src/db/settings/BlocksSetting.ts
+++ src/db/settings/BlocksSetting.ts
@@ -4,7 +4,6 @@ export const BlocksSetting = new Setting<boolean>(
     'blocks',
     true,
     false,
-    // Deactivating blocks mode for now; it's too unstable.
-    (value) => false, //(typeof value === 'boolean' ? value : false),
+    (value) => (typeof value === 'boolean' ? value : false),
     (current, value) => current === value,
 );
