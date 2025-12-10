7fe8e250e323670d585bcbb8f78b278306ac0677
Melissa Chen <31445526+melissachen2000@users.noreply.github.com>
2025-12-10 16:34:45 +0800

    Sketching UI for drafts space

--- src/routes/gallery/[galleryid]/howto/HowToDrafts.svelte
+++ src/routes/gallery/[galleryid]/howto/HowToDrafts.svelte
@@ -1,3 +1,21 @@
-<p>
-    Hello world from drafts!
-</p>
+<script>
+    import Subheader from '@components/app/Subheader.svelte';
+    import MarkupHTMLView from '@components/concepts/MarkupHTMLView.svelte';
+</script>
+
+<div class="draftspace">
+    <Subheader text={(l) => l.ui.howto.draftsheader} />
+    <MarkupHTMLView markup={(l) => l.ui.howto.draftsprompt} />
+</div>
+
+<style>
+    .draftspace {
+        border: var(--wordplay-border-color);
+        border-radius: var(--wordplay-border-radius);
+        border-style: dashed;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: var(--wordplay-spacing);
+    }
+</style>
