477d8e37938a5d05d58b20770e79a7e73ee06e37
Amy J. Ko <ajko@uw.edu>
2025-12-22 15:34:11 -0800

    Added menu for type placeholder.

--- src/components/editor/nodes/TypePlaceholderView.svelte
+++ src/components/editor/nodes/TypePlaceholderView.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import { getCaret } from '@components/project/Contexts';
     import type TypePlaceholder from '@nodes/TypePlaceholder';
+    import MenuTrigger from '../menu/MenuTrigger.svelte';
     import NodeView, { type Format } from './NodeView.svelte';
 
     interface Props {
@@ -8,6 +10,9 @@
     }
 
     let { node, format }: Props = $props();
+
+    const caret = getCaret();
 </script>
 
 <NodeView node={[node, 'placeholder']} {format} />
+{#if caret && format.block}<MenuTrigger anchor={node} />{/if}
