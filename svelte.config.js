8c4dc94f2d618a718ef8093c6783cfcc706576ea
Amy J. Ko <ajko@uw.edu>
2026-02-07 09:52:42 -0800

    Fixed svelte preprocessor.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import path from 'path';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    // preprocess: sveltePreprocess(),
-
+    preprocess: sveltePreprocess(),
     kit: {
         adapter: adapter({
             pages: 'build',
