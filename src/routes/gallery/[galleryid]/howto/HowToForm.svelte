8d26e7e97ef558084efdc65587d38983fce5ea3a
Melissa Chen <31445526+melissachen2000@users.noreply.github.com>
2026-03-11 21:48:58 -0500

    CSS fixes for social features height

--- src/routes/gallery/[galleryid]/howto/HowToForm.svelte
+++ src/routes/gallery/[galleryid]/howto/HowToForm.svelte
@@ -823,13 +823,13 @@
             />
         </div>
         <div class="howtosplitview">
-            <div class="splitside" id="howtoview">
+            <div class="how-to-text" id="howtoview">
                 {#each howTo.getText() as markup, i (i)}
                     <HowToPrompt text={(l) => prompts[i]} />
                     <MarkupHTMLView {markup} />
                 {/each}
             </div>
-            <div class="splitside" id="howtointeractions">
+            <div class="how-to-social" id="howtointeractions">
                 <HowToPrompt text={(l) => l.ui.howto.viewer.reactionsPrompt} />
                 {#each reactionButtons as reaction, i (i)}
                     <Button
@@ -852,12 +852,14 @@
 
                 <HowToPrompt text={(l) => l.ui.howto.viewer.chatPrompt} />
 
-                <ChatView
-                    {chat}
-                    creators={chatParticipants}
-                    {galleryID}
-                    {howTo}
-                />
+                <div class="how-to-chat">
+                    <ChatView
+                        {chat}
+                        creators={chatParticipants}
+                        {galleryID}
+                        {howTo}
+                    />
+                </div>
             </div>
         </div>
     {:else if howTo && (!$user || !isCreatorCollaboratorViewer($user.uid))}
@@ -948,16 +950,23 @@
         grid-template-columns: 1fr 1fr;
         gap: var(--wordplay-spacing);
         height: 100%;
-        overflow: hidden;
     }
 
-    .splitside {
+    .how-to-text {
         height: 100%;
-        max-height: 100%;
         width: 100%;
         padding: var(--wordplay-spacing);
-        overflow-y: auto;
-        overscroll-behavior-y: contain;
+    }
+
+    .how-to-social {
+        height: 100%;
+        width: 100%;
+        padding: var(--wordplay-spacing);
+    }
+
+    .how-to-chat {
+        height: 100%;
+        max-height: 50vh;
     }
 
     .optionsarea {
