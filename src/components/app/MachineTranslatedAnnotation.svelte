daa2790a98008c55bae7112c5a712afadb5494de
Amy J. Ko <ajko@uw.edu>
2026-04-04 17:28:02 -0700

    Wiggle machine translation annotations when in localizing mode.

--- src/components/app/MachineTranslatedAnnotation.svelte
+++ src/components/app/MachineTranslatedAnnotation.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    import { getTip } from '@components/project/Contexts';
+    import { getLocalizing, getTip } from '@components/project/Contexts';
     import { locales } from '@db/Database';
     import { MACHINE_TRANSLATED_SYMBOL } from '@parser/Symbols';
     import Emoji from './Emoji.svelte';
 
     let hint = getTip();
+    let localize = getLocalizing();
 
     let annotation: HTMLSpanElement | undefined = undefined;
 
@@ -25,6 +26,7 @@
         role="tooltip"
         bind:this={annotation}
         aria-label={tip}
+        class:wiggle={localize.on}
         onfocus={showTip}
         onblur={hideTip}
         onpointerenter={showTip}
@@ -36,5 +38,24 @@
     span {
         font-size: 8pt;
         cursor: help;
+    }
+
+    @keyframes wiggle {
+        0%,
+        100% {
+            transform: rotate(0deg) scale(2);
+        }
+        25% {
+            transform: rotate(-15deg) scale(1.8);
+        }
+        75% {
+            transform: rotate(15deg) scale(2);
+        }
+    }
+
+    .wiggle {
+        display: inline-block;
+        animation: wiggle 0.6s ease-in-out infinite;
+        color: var(--wordplay-warning);
     }
 </style>
