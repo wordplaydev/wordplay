aec4707d2b3b6a0b42769aadcd62e8bfa85b09c1
Amy J. Ko <ajko@uw.edu>
2026-04-11 12:06:37 -0700

    Updated to TypeScript 6 in functions.

---
+++ functions/src/index.js
@@ -0,0 +1,448 @@
+import Translate from '@google-cloud/translate';
+import { PromisePool } from '@supercharge/promise-pool';
+import admin from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { defineString } from 'firebase-functions/params';
+import { onDocumentCreated, onDocumentWritten } from 'firebase-functions/v2/firestore';
+import { onCall, onRequest } from 'firebase-functions/v2/https';
+import { onSchedule } from 'firebase-functions/v2/scheduler';
+import * as http from 'http';
+import * as https from 'https';
+import nodemailer from 'nodemailer';
+initializeApp();
+const db = getFirestore();
+// Permit local testing and calls from our two domains.
+const cors = {
+    cors: [
+        '/firebase\.com$/',
+        '/127.0.0.1*/',
+        'http://localhost:5173',
+        'https://test.wordplay.dev',
+        'https://wordplay.dev',
+    ],
+};
+export const getCreators = onCall(cors, async (request) => {
+    const users = await admin.auth().getUsers(request.data);
+    const matches = [];
+    users.users.forEach((user) => {
+        var _a, _b;
+        matches.push({
+            email: (_a = user.email) !== null && _a !== void 0 ? _a : null,
+            uid: user.uid,
+            name: (_b = user.displayName) !== null && _b !== void 0 ? _b : null,
+        });
+    });
+    return matches;
+});
+/** Given a list of email addresses, return a map email => boolean indicating whether there is a corresponding account exists. Maximum of 100.*/
+export const emailExists = onCall(cors, async (request) => {
+    const emails = request.data;
+    return admin
+        .auth()
+        .getUsers(emails.map((e) => {
+        return { email: e };
+    }))
+        .then(({ users }) => {
+        const found = {};
+        for (const email of emails)
+            found[email] = users.some((u) => u.email === email);
+        return found;
+    })
+        .catch(() => {
+        return undefined;
+    });
+});
+/**
+ * Given a from to locale (using ll, where ll is a two character language code),
+ * and a list of strings, use Google Cloud Translate to translate the list of strings into the target language.)
+ */
+export const getTranslations = onCall(cors, async (request) => {
+    const from = request.data.from;
+    const to = request.data.to;
+    const text = request.data.text;
+    try {
+        // Creates a Google Translate client
+        const translator = new Translate.v2.Translate();
+        const [translations] = await translator.translate(text, {
+            from,
+            to,
+        });
+        return translations;
+    }
+    catch (e) {
+        console.error(e);
+        return null;
+    }
+});
+/** Given a URL that should refer to an HTML document, sends a GET request to the URL to try to get the document's text. */
+export const getWebpage = onRequest(cors, async (request, response) => {
+    const url = 'url' in request.query && typeof request.query.url === 'string'
+        ? decodeURI(request.query['url'])
+        : undefined;
+    const lib = url === undefined
+        ? undefined
+        : url.startsWith('https://')
+            ? https
+            : http;
+    // Cache the response for 10 minutes to minimize requests.
+    response.set('Cache-Control', 'public, max-age=600, s-maxage=600');
+    if (lib === undefined || url === undefined) {
+        console.log('Invalid URL ' + url);
+        response.json('invalid-url');
+    }
+    else {
+        const result = await new Promise((resolve) => {
+            lib.get(url, (resp) => {
+                const contentType = resp.headers['content-type'];
+                if (resp.statusCode !== 200) {
+                    console.error(`GET status: Code: ${resp.statusCode}`);
+                    resolve('not-available');
+                }
+                else if (!(contentType === null || contentType === void 0 ? void 0 : contentType.startsWith('text/html'))) {
+                    console.error(`GET received ${contentType}`);
+                    resolve('not-html');
+                }
+                // Get the data
+                let data = '';
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    console.log('GET success');
+                    resolve(data);
+                });
+            }).on('error', (err) => {
+                console.error('GET error: ' + err.message);
+                resolve('not-available');
+            });
+        });
+        // Set the content length header.
+        response.set('Content-Length', `${new Blob([result]).size}`);
+        // Send the HTML back as JSON-encoded text.
+        response.json(result);
+    }
+});
+const PurgeDayDelay = 30;
+const MillisecondsPerDay = 24 * 60 * 60 * 1000;
+/** Every day, delete projects that were archived more than 30 days ago. */
+export const purgeArchivedProjects = onSchedule({ schedule: 'every day 00:00', timeZone: 'UTC' }, async () => {
+    // Fetch all archived projects that were last modified more than 30 days ago
+    const projectsRef = db.collection('projects');
+    const purgeable = await projectsRef
+        .where('archived', '==', true)
+        .where('timestamp', '<', Date.now() - PurgeDayDelay * MillisecondsPerDay)
+        .get();
+    const projectIDs = [];
+    purgeable.forEach((doc) => projectIDs.push(doc.id));
+    // Don't delete all at once; we'll hit a request limit on large purges.
+    await PromisePool.for(projectIDs)
+        .withConcurrency(3)
+        .process(async (id) => {
+        projectsRef.doc(id).delete();
+    });
+});
+/**
+ * Given a teacher user ID, credential information for several students, and
+ * a name and description for a class, create a class and return it's ID
+ */
+export const createClass = onCall(cors, async (request) => {
+    var _a;
+    const auth = admin.auth();
+    const { teacher, name, description, students, existing } = request.data;
+    // Make sure there aren't too many students.
+    if (students.length > 50)
+        return { classid: undefined, error: { kind: 'limit', info: '' } };
+    // Ensure the teacher is a valid user ID.
+    try {
+        await auth.getUser(teacher);
+    }
+    catch (error) {
+        console.error(JSON.stringify(error));
+        return {
+            classid: undefined,
+            undefined,
+            error: {
+                kind: 'generic',
+                info: "The teacher user id provided doesn't exist",
+            },
+        };
+    }
+    // Ensure each existing user is a valid user ID and get their usernames.
+    const existingEmails = new Map();
+    for (const uid of existing) {
+        try {
+            const user = await auth.getUser(uid);
+            existingEmails.set(uid, (_a = user.email) !== null && _a !== void 0 ? _a : '');
+        }
+        catch (error) {
+            console.error(JSON.stringify(error));
+            return {
+                classid: undefined,
+                error: {
+                    kind: 'generic',
+                    info: "One of the existing user ids provided doesn't exist",
+                },
+            };
+        }
+    }
+    // Ensure the students are the correct type
+    if (!Array.isArray(students)) {
+        console.error('Received', JSON.stringify(students));
+        return {
+            classid: undefined,
+            error: {
+                kind: 'generic',
+                info: `expected a list of students but received a ${typeof students}`,
+            },
+        };
+    }
+    const malformed = students.some((s) => typeof s.username !== 'string' ||
+        typeof s.password !== 'string' ||
+        !Array.isArray(s.meta));
+    if (malformed)
+        return {
+            classid: undefined,
+            error: {
+                kind: 'generic',
+                info: `expected students in the list to have a username, password, and list of text, received ${JSON.stringify(malformed)}`,
+            },
+        };
+    // Verify that none of the user accounts exist
+    try {
+        const result = await auth.getUsers(students.map((s) => {
+            return { email: s.username };
+        }));
+        // If any users exist, then we bail.
+        if (result.users.length > 0)
+            return {
+                classid: undefined,
+                error: {
+                    kind: 'account',
+                    info: 'One or more students already have accounts',
+                },
+            };
+    }
+    catch (err) {
+        return {
+            classid: undefined,
+            error: {
+                kind: 'generic',
+                info: `Unable to check for existing users: ${JSON.stringify(err)}`,
+            },
+        };
+    }
+    // Okay, we're ready to create the user accounts!
+    const users = [];
+    for (const student of students) {
+        try {
+            const user = await auth.createUser({
+                email: student.username,
+                password: student.password,
+            });
+            users.push({
+                uid: user.uid,
+                username: student.username,
+                meta: student.meta,
+            });
+        }
+        catch (error) {
+            return {
+                classid: undefined,
+                error: {
+                    kind: 'generic',
+                    info: `Unable to create user: ${JSON.stringify(error)}`,
+                },
+            };
+        }
+    }
+    // Create the class
+    const classRef = db.collection('classes').doc();
+    await classRef.set({
+        id: classRef.id,
+        name,
+        description,
+        teachers: [teacher],
+        learners: [...existing, ...users.map((u) => u.uid)],
+        // Convert the email address back to a username
+        info: [
+            ...users.map((u) => {
+                return Object.assign(Object.assign({}, u), { username: u.username.split('@')[0] });
+            }),
+            // Ensure there's a row for each existing student
+            ...existing.map((uid) => {
+                var _a, _b, _c;
+                return {
+                    uid,
+                    username: (_a = existingEmails.get(uid)) !== null && _a !== void 0 ? _a : '',
+                    // Ensure the meta array is the same length as the new users
+                    meta: (_c = (_b = users[0]) === null || _b === void 0 ? void 0 : _b.meta.map(() => '')) !== null && _c !== void 0 ? _c : [],
+                };
+            }),
+        ],
+        galleries: [],
+    });
+    return { classid: classRef.id, error: undefined };
+});
+const emailPassword = defineString('SMTP_PASSWORD');
+/** When new feedback is created, post it to the GitHub repository. */
+export const postFeedback = onDocumentCreated('feedback/{id}', async (event) => {
+    var _a;
+    const feedback = (_a = event.data) === null || _a === void 0 ? void 0 : _a.data();
+    if (feedback === undefined)
+        return;
+    if (feedback.title === undefined || feedback.description === undefined)
+        return;
+    let authData = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'hi@wordplay.dev',
+            pass: emailPassword.value(),
+        },
+    });
+    return authData.sendMail({
+        from: 'hi@wordplay.dev',
+        to: 'hi@wordplay.dev',
+        subject: `New feedback`,
+        text: `New feedback from the app:\n\n${feedback.title}\n\n${feedback.description}`,
+        html: `<p>New feedback from the app!</p><h2>Title</h2><p>${feedback.title}</p><h2>Description</h2><p>${feedback.description}</p>`,
+    });
+});
+export const galleryEdited = onDocumentWritten('galleries/{id}', async (event) => {
+    var _a, _b;
+    const before = (_a = event.data) === null || _a === void 0 ? void 0 : _a.before.data();
+    const after = (_b = event.data) === null || _b === void 0 ? void 0 : _b.after.data();
+    const howToStore = db.collection('howtos');
+    const galleryStore = db.collection('galleries');
+    const batch = db.batch();
+    const listEq = (a, b) => {
+        if (a.length !== b.length)
+            return false;
+        const aSorted = [...a].sort();
+        const bSorted = [...b].sort();
+        for (let i = 0; i < aSorted.length; i++) {
+            if (aSorted[i] !== bSorted[i])
+                return false;
+        }
+        return true;
+    };
+    // if deletion or if none of the relevant fields changed, then return to prevent infinite loops
+    if (!after || (before && listEq(before.curators, after.curators) && listEq(before.creators, after.creators) && listEq(before.howToViewersFlat, after.howToViewersFlat) && before.howToExpandedVisibility === after.howToExpandedVisibility && listEq(before.howTos, after.howTos))) {
+        return;
+    }
+    // if the gallery was just created or the list of curators have changed,
+    // then we need to update the curators' other galleries' lists of expanded galleries and viewers
+    if (!before || !listEq(before.curators, after.curators)) {
+        const removedCurators = before ? before.curators.filter((c) => !after.curators.includes(c)) : [];
+        const addedCurators = before ? after.curators.filter((c) => !before.curators.includes(c)) : after.curators;
+        if (removedCurators.length > 0) {
+            const galleriesToUpdate = await galleryStore.where('curators', 'array-contains-any', removedCurators).get();
+            galleriesToUpdate.forEach((g) => {
+                const otherGallery = g.data();
+                if (otherGallery.id !== after.id && !otherGallery.curators.some((c) => after.curators.includes(c))) {
+                    // if none of the curators remain, then remove this gallery from the expanded galleries and viewer lists
+                    let howToExpandedGalleries = otherGallery.howToExpandedGalleries.filter((id) => id !== after.id);
+                    let howToViewers = otherGallery.howToViewers;
+                    delete howToViewers[after.id];
+                    let howToViewersFlat = Object.values(howToViewers).flat();
+                    batch.update(galleryStore.doc(g.id), {
+                        howToExpandedGalleries: howToExpandedGalleries,
+                        howToViewers: howToViewers,
+                        howToViewersFlat: howToViewersFlat,
+                    });
+                }
+            });
+        }
+        if (addedCurators.length > 0) {
+            const galleriesToUpdate = await galleryStore.where('curators', 'array-contains-any', addedCurators).get();
+            galleriesToUpdate.forEach((g) => {
+                const otherGallery = g.data();
+                if (otherGallery.id !== after.id) {
+                    let howToExpandedGalleries = otherGallery.howToExpandedGalleries.includes(after.id) ? otherGallery.howToExpandedGalleries : [...otherGallery.howToExpandedGalleries, after.id];
+                    let howToViewers = otherGallery.howToViewers;
+                    howToViewers[after.id] = [...after.curators, ...after.creators];
+                    let howToViewersFlat = Object.values(howToViewers).flat();
+                    batch.update(galleryStore.doc(g.id), {
+                        howToExpandedGalleries: howToExpandedGalleries,
+                        howToViewers: howToViewers,
+                        howToViewersFlat: howToViewersFlat,
+                    });
+                }
+            });
+        }
+    }
+    else if (!listEq(before.creators, after.creators)) {
+        // else if the gallery's list of creators have changed
+        // then we need to update the viewer lists of all of the other galleries, too
+        const galleriesToUpdate = await galleryStore.where('curators', 'array-contains-any', after.curators).get();
+        galleriesToUpdate.forEach(async (g) => {
+            const otherGallery = g.data();
+            if (otherGallery.id !== after.id) {
+                let howToViewers = otherGallery.howToViewers;
+                howToViewers[after.id] = [...after.curators, ...after.creators];
+                let howToViewersFlat = Object.values(howToViewers).flat();
+                if (otherGallery.v === 2) {
+                    batch.update(galleryStore.doc(g.id), {
+                        howToViewers: howToViewers,
+                        howToViewersFlat: howToViewersFlat,
+                    });
+                }
+                else {
+                    await galleryStore.doc(g.id).update({
+                        v: 2,
+                        howTos: [],
+                        howToExpandedVisibility: false,
+                        howToExpandedGalleries: [after.id],
+                        howToViewers: howToViewers,
+                        howToViewersFlat: howToViewersFlat,
+                    });
+                }
+            }
+        });
+    }
+    // if expanded scope is on orlist of how-tos was updated, then we need to update their viewer permissions
+    if (before && (before.howToExpandedVisibility !== after.howToExpandedVisibility || (after.howToExpandedVisibility && !listEq(before.howTos, after.howTos)))) {
+        if (after.howToExpandedVisibility) {
+            after.howTos.forEach(async (howToID) => {
+                batch.update(howToStore.doc(howToID), {
+                    viewers: after.howToViewers,
+                    viewersFlat: after.howToViewersFlat,
+                });
+            });
+        }
+        else {
+            after.howTos.forEach(async (howToID) => {
+                batch.update(howToStore.doc(howToID), {
+                    viewers: {},
+                    viewersFlat: [],
+                });
+            });
+        }
+    }
+    // if document was just created, then update its own list of expanded galleries and viewers
+    if (!before) {
+        let howToExpandedGalleries = [];
+        let howToViewers = {};
+        let howToViewersFlat = [];
+        const otherGalleries = await galleryStore.where('curators', 'array-contains-any', after.curators).get();
+        otherGalleries.forEach((g) => {
+            const otherGallery = g.data();
+            if (otherGallery.id !== after.id) {
+                howToExpandedGalleries.push(otherGallery.id);
+                howToViewers[otherGallery.id] = [...otherGallery.curators, ...otherGallery.creators];
+            }
+        });
+        howToViewersFlat = Object.values(howToViewers).flat();
+        batch.update(galleryStore.doc(after.id), {
+            howToExpandedGalleries: howToExpandedGalleries,
+            howToViewers: howToViewers,
+            howToViewersFlat: howToViewersFlat,
+        });
+    }
+    return batch.commit();
+});
+//# sourceMappingURL=index.js.map
