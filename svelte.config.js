d67e94dbfda268e965a703ea8fa1946c7062c1fc
Amy J. Ko <ajko@uw.edu>
2026-04-19 11:02:20 -0700

    Resolved fallback warning in SPA.

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
         adapter: adapter({
             pages: 'build',
             assets: 'build',
-            fallback: 'index.html',
+            fallback: '200.html',
         }),
         alias: {
             '@components': path.resolve('./src/components'),
