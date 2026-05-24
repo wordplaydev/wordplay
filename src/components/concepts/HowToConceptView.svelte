270d88afa982e4e263c875dd280ede0809272ec2
Melissa Chen <31445526+melissachen2000@users.noreply.github.com>
2026-05-24 17:59:16 -0500

    Increment view count from guide

--- src/components/concepts/HowToConceptView.svelte
+++ src/components/concepts/HowToConceptView.svelte
@@ -2,9 +2,12 @@
     import { goto } from '$app/navigation';
     import Subheader from '@components/app/Subheader.svelte';
     import Speech from '@components/lore/Speech.svelte';
+    import { getUser } from '@components/project/Contexts';
     import Button from '@components/widgets/Button.svelte';
     import type GalleryHowConcept from '@concepts/GalleryHowConcept';
-    import { locales } from '@db/Database';
+    import { HowTos, locales } from '@db/Database';
+    import HowTo from '@db/howtos/HowToDatabase.svelte';
+    import { onMount } from 'svelte';
     import MarkupHTMLView from './MarkupHTMLView.svelte';
 
     interface Props {
@@ -14,6 +17,29 @@
     let { concept }: Props = $props();
 
     let preferredLocale: string = $derived($locales.getLocaleString());
+
+    // add the user to the list of those who have viewed this how-to
+    const user = getUser();
+
+    onMount(() => {
+        let seenByUsers = concept.howTo.getSeenByUsers();
+
+        if ($user && !seenByUsers.includes($user.uid)) {
+            seenByUsers.push($user.uid);
+        }
+
+        HowTos.updateHowTo(
+            new HowTo({
+                ...concept.howTo.getData(),
+                social: {
+                    ...concept.howTo.getSocial(),
+                    seenByUsers: seenByUsers,
+                    viewCount: concept.howTo.getViewCount() + 1,
+                },
+            }),
+            true,
+        );
+    });
 </script>
 
 <Subheader>{concept.howTo.getTitleInLocale(preferredLocale)}</Subheader>
